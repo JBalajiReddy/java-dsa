a4ad2f572d190a89e8a6c31828b5288e00af929f
JBalajiReddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-02-18 08:00:27 +0530

    construct-smallest-number-from-di-string.java

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -1,3 +1,4 @@
+//recursion
 class Solution {
     public String smallestNumber(String pattern) {
         StringBuilder res = new StringBuilder();
@@ -15,5 +16,22 @@ class Solution {
         }
         res.append(currCount + 1);
         return currCount + 1;
+    }
+}
+
+//Stack
+class Solution {
+    public String smallestNumber(String pattern) {
+        StringBuilder res = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i <= pattern.length(); i++) {
+            stack.push(i + 1);
+            if (i == pattern.length() || pattern.charAt(i) == 'I') {
+                while (!stack.isEmpty())
+                    res.append(stack.pop());
+            }
+        }
+        return res.toString();
     }
 }
