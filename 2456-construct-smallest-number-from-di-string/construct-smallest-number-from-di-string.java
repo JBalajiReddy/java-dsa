3610363bc82ccade375b0ee4e6c8f582a7a44bad
JBalajiReddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-02-18 08:01:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (36.89%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -19,19 +19,19 @@ class Solution {
     }
 }
 
-//Stack
-class Solution {
-    public String smallestNumber(String pattern) {
-        StringBuilder res = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
+// //Stack
+// class Solution {
+//     public String smallestNumber(String pattern) {
+//         StringBuilder res = new StringBuilder();
+//         Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i <= pattern.length(); i++) {
-            stack.push(i + 1);
-            if (i == pattern.length() || pattern.charAt(i) == 'I') {
-                while (!stack.isEmpty())
-                    res.append(stack.pop());
-            }
-        }
-        return res.toString();
-    }
-}
+//         for (int i = 0; i <= pattern.length(); i++) {
+//             stack.push(i + 1);
+//             if (i == pattern.length() || pattern.charAt(i) == 'I') {
+//                 while (!stack.isEmpty())
+//                     res.append(stack.pop());
+//             }
+//         }
+//         return res.toString();
+//     }
+// }
