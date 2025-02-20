6b87adb450140543242b667aa67c9c443ed4f9f0
JBalajiReddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-02-20 07:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (93.86%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,27 +1,39 @@
+//Cantor's diagonal argument
 class Solution {
-    int n;
-    Set<String> set = new HashSet<>();
-
     public String findDifferentBinaryString(String[] nums) {
-        n = nums.length;
-        for (String s : nums) {
-            set.add(s);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            Character ch = nums[i].charAt(i);
+            sb.append(ch == '0' ? '1' : '0');
         }
-        return recur("");
-    }
-
-    public String recur(String currStr) {
-        if (currStr.length() == n) {
-            if (!set.contains(currStr)) {
-                return currStr;
-            }
-            return "";
-        }
-        String addZero = recur(currStr + "0");
-
-        if (addZero.length() > 0)
-            return addZero;
-
-        return recur(currStr + "1");
+        return sb.toString();
     }
 }
+
+// class Solution {
+// int n;
+// Set<String> set = new HashSet<>();
+
+// public String findDifferentBinaryString(String[] nums) {
+// n = nums.length;
+// for (String s : nums) {
+// set.add(s);
+// }
+// return recur("");
+// }
+
+// public String recur(String currStr) {
+// if (currStr.length() == n) {
+// if (!set.contains(currStr)) {
+// return currStr;
+// }
+// return "";
+// }
+// String addZero = recur(currStr + "0");
+
+// if (addZero.length() > 0)
+// return addZero;
+
+// return recur(currStr + "1");
+// }
+// }
