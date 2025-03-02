a9a93549d60d812ebd2e25ebbc6252623d430d0a
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-03-02 12:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (96.71%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,20 +1,53 @@
+//two pointers
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num[] : nums1) {
-            map.put(num[0], num[1]);
+        int n1 = nums1.length, n2 = nums2.length, i = 0, j = 0;
+        ArrayList<int[]> ans = new ArrayList();
+        while (i < n1 && j < n2) {
+            if (nums1[i][0] == nums2[j][0]) {
+                ans.add(new int[] { nums1[i][0], nums1[i][1] + nums2[j][1] });
+                i++;
+                j++;
+            } else if (nums1[i][0] < nums2[j][0]) {
+                ans.add(nums1[i]);
+                i++;
+            } else {
+                ans.add(nums2[j]);
+                j++;
+            }
         }
-        for (int num[] : nums2) {
-            map.put(num[0], map.getOrDefault(num[0], 0) + num[1]);
+        while (i < n1) {
+            ans.add(nums1[i++]);
         }
-
-        List<int[]> list = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            list.add(new int[] { entry.getKey(), entry.getValue() });
+        while (j < n2) {
+            ans.add(nums2[j++]);
         }
-
-        list.sort(Comparator.comparingInt(a -> a[0]));
-
-        return list.toArray(new int[list.size()][2]);
+        return ans.toArray(new int[ans.size()][]);
     }
 }
+
+
+
+
+
+// HashMap
+// class Solution {
+//     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for (int num[] : nums1) {
+//             map.put(num[0], num[1]);
+//         }
+//         for (int num[] : nums2) {
+//             map.put(num[0], map.getOrDefault(num[0], 0) + num[1]);
+//         }
+
+//         List<int[]> list = new ArrayList<>();
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//             list.add(new int[] { entry.getKey(), entry.getValue() });
+//         }
+
+//         list.sort(Comparator.comparingInt(a -> a[0]));
+
+//         return list.toArray(new int[list.size()][2]);
+//     }
+// }
