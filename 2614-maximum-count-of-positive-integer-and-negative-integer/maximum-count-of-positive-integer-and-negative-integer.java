9d41a6d5f6f24bb4f07cf7bb11dbc98b5077013d
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-03-12 12:06:52 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (63.30%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,10 +1,45 @@
+//Binary Search - search 0's lowerbound and upperbound
 class Solution {
     public int maximumCount(int[] nums) {
-        int pos = 0, neg = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] < 0) neg++;
-            if(nums[i] > 0) pos++;
+        return Math.max(lowerBound(nums), nums.length - upperBound(nums));
+    }
+    public int lowerBound(int[] nums) {
+        int low = 0, high = nums.length - 1, mid = 0, idx = nums.length;
+        while(low <= high) {
+            mid = low + (high  - low) / 2;
+            if(nums[mid] < 0) {
+                low = mid + 1;
+            } else if(nums[mid] >= 0) {
+                high = mid - 1;
+                idx = mid;
+            }
+        } 
+        return idx;
+    }
+    public int upperBound(int[] nums) {
+        int low = 0, high = nums.length - 1, mid = 0, idx = nums.length;
+        while(low <= high) {
+            mid = low + (high  - low) / 2;
+            if(nums[mid] <= 0) {
+                low = mid + 1;
+            } else if(nums[mid] > 0) {
+                high = mid - 1;
+                idx = mid;
+            }
         }
-        return Math.max(pos, neg);
+        return idx;
     }
 }
+
+
+// Brute Force
+// class Solution {
+//     public int maximumCount(int[] nums) {
+//         int pos = 0, neg = 0;
+//         for(int i = 0; i < nums.length; i++) {
+//             if(nums[i] < 0) neg++;
+//             if(nums[i] > 0) pos++;
+//         }
+//         return Math.max(pos, neg);
+//     }
+// }
