c6082a2213c13ef01587966697f688aeabad7166
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-03-13 23:15:19 +0530

    Time: 17 ms (66.29%) | Memory: 91.5 MB (99.15%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries, int k) {
-        int n = nums.length, sum = 0; 
+        int n = nums.length, sum = 0;
         int[] diff = new int[n + 1];
 
-        for(int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i++) {
             int left = queries[i][0];
             int right = queries[i][1];
             int val = queries[i][2];
 
             diff[left] -= val;
-            if(right + 1 < n) 
-               diff[right + 1] += val;
+            if (right + 1 < n)
+                diff[right + 1] += val;
         }
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             sum += diff[i];
-            if(nums[i] + sum > 0) return false;
+            if (nums[i] + sum > 0)
+                return false;
         }
         return true;
     }
@@ -22,13 +23,16 @@ class Solution {
     public int minZeroArray(int[] nums, int[][] queries) {
         int left = 0, right = queries.length;
         // Check if it's possible to make the array zero with all queries
-        if(!isZeroArray(nums, queries, right)) return -1;
-        
+        if (!isZeroArray(nums, queries, right))
+            return -1;
+
         // Binary search to find the minimum number of queries needed
-        while(left <= right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if(isZeroArray(nums, queries, mid)) right = mid -1;
-            else left = mid + 1;
+            if (isZeroArray(nums, queries, mid))
+                right = mid - 1;
+            else
+                left = mid + 1;
         }
         return left;
     }
