63d6200582686f5adc8805130eecfa120097086f
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-03-13 22:17:28 +0530

    Time: 3 ms (80.91%) | Memory: 94.9 MB (67.31%) - LeetSync

---
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.java
@@ -0,0 +1,23 @@
+//prefix sum
+class Solution {
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int[] diff = new int[n + 1];
+        for (int idx[] : queries) {
+            int l = idx[0];
+            int r = idx[1];
+            diff[l]--;
+            if (r + 1 < n)
+                diff[r + 1]++;
+        }
+        int decCount = 0;
+        for (int i = 0; i < n; i++) {
+            decCount += diff[i];
+            if (decCount + nums[i] > 0) {
+                return false;
+                //number cannot be reduced to zero 
+            }
+        }
+        return true;
+    }
+}
