ae6877e332d1a6a9efa2264837c7ef015915c7a3
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-03-13 23:07:27 +0530

    Time: 19 ms (55.24%) | Memory: 91.5 MB (98.87%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isZeroArray(int[] nums, int[][] queries, int k) {
+        int n = nums.length, sum = 0; 
+        int[] diff = new int[n + 1];
+
+        for(int i = 0; i < k; i++) {
+            int left = queries[i][0];
+            int right = queries[i][1];
+            int val = queries[i][2];
+
+            diff[left] -= val;
+            if(right + 1 < n) 
+               diff[right + 1] += val;
+        }
+        for(int i = 0; i < n; i++) {
+            sum += diff[i];
+            if(nums[i] + sum > 0) return false;
+        }
+        return true;
+    }
+
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int left = 0, right = queries.length;
+        if(!isZeroArray(nums, queries, right)) return -1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(isZeroArray(nums, queries, mid)) right = mid -1;
+            else left = mid + 1;
+        }
+        return left;
+    }
+}
