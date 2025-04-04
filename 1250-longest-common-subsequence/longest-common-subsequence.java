d918a32c1057b9c4bb3a80c0bc74701f83e67ef5
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-04-04 14:44:07 +0530

    Time: 20 ms (75.21%) | Memory: 51.1 MB (30.43%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,22 +1,46 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        //dp[0][0] = 0;
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+        int n = text1.length();
+        int m = text2.length();
+        int dp[][] = new int[n + 1][m + 1];
+        dp[0][0] = 0;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else
-                    //case 1 : Excluding the current character of text1: dp[i - 1][j]
-                    //case 2 :  Excluding the current character of text2: dp[i][j - 1]
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    int case1 = dp[i - 1][j];
+                    int case2 = dp[i][j - 1];
+                    dp[i][j] = Math.max(case1, case2);
+                }
             }
         }
-        return dp[m][n];
+        return dp[n][m];
     }
 }
+
+
+
+
+// class Solution {
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         int m = text1.length();
+//         int n = text2.length();
+//         int[][] dp = new int[m + 1][n + 1];
+//         dp[0][0] = 0;
+//         for (int i = 1; i <= m; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+//                     dp[i][j] = dp[i - 1][j - 1] + 1;
+//                 } else
+//                     case 1 : Excluding the current character of text1: dp[i - 1][j]
+//                     case 2 :  Excluding the current character of text2: dp[i][j - 1]
+//                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// }
 
 // TLE
 // class Solution {
