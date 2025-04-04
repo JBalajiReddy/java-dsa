31e2a25b47479df699571e53bb281ce60c62a823
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-04-04 14:32:45 +0530

    Time: 20 ms (75.21%) | Memory: 50.8 MB (67.04%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,14 +3,14 @@ class Solution {
         int m = text1.length();
         int n = text2.length();
         int[][] dp = new int[m + 1][n + 1];
-
+        //dp[0][0] = 0;
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else
-                    // Excluding the current character of text1: dp[i - 1][j]
-                    // Excluding the current character of text2: dp[i][j - 1]
+                    //case 1 : Excluding the current character of text1: dp[i - 1][j]
+                    //case 2 :  Excluding the current character of text2: dp[i][j - 1]
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
