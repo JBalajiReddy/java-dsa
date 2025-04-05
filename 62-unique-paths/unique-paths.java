b1a6d6847438d3d8f7063392b45bb83677be4f6d
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-04-05 12:38:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (76.57%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -4,9 +4,9 @@ class Solution {
         int dp[][] = new int[m + 1][n + 1];
         for (int i = 0; i < m + 1; i++) {
             for (int j = 0; j < n + 1; j++) {
-                if (i == 0 || j == 0) {
+                if (i == 0 || j == 0) {  //out of bound cells
                     dp[i][j] = 0;
-                } else if (i == 1 && j == 1) {
+                } else if (i == 1 && j == 1) {   //1 way to reach starting cells
                     dp[i][j] = 1;
                 } else {
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
