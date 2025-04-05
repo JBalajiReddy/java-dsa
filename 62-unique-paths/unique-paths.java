32dad4d6a1287fff891d75cdd075afadfca116c6
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-04-05 12:25:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (26.24%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,25 +1,46 @@
-//top down
+//bottom up
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m + 1][n + 1];
         for (int i = 0; i < m + 1; i++) {
-            Arrays.fill(dp[i], -1);
+            for (int j = 0; j < n + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (i == 1 && j == 1) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
         }
-        return topDownDP(m, n, dp);
-    }
-
-    private int topDownDP(int m, int n, int[][] dp) {
-        if (m == 0 || n == 0) {
-            dp[m][n] = 0;
-            return 0;
-        } else if (m == 1 && n == 1) {
-            dp[m][n] = 1;
-            return 1;
-        }
-        if (dp[m][n] != -1) {
-            return dp[m][n];
-        }
-        dp[m][n] = topDownDP(m - 1, n, dp) + topDownDP(m, n - 1, dp);
         return dp[m][n];
     }
 }
+
+
+
+// top down
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int dp[][] = new int[m + 1][n + 1];
+//         for (int i = 0; i < m + 1; i++) {
+//             Arrays.fill(dp[i], -1);
+//         }
+//         return topDownDP(m, n, dp);
+//     }
+
+//     private int topDownDP(int m, int n, int[][] dp) {
+//         if (m == 0 || n == 0) {
+//             dp[m][n] = 0;
+//             return 0;
+//         } else if (m == 1 && n == 1) {
+//             dp[m][n] = 1;
+//             return 1;
+//         }
+//         if (dp[m][n] != -1) {
+//             return dp[m][n];
+//         }
+//         dp[m][n] = topDownDP(m - 1, n, dp) + topDownDP(m, n - 1, dp);
+//         return dp[m][n];
+//     }
+// }
