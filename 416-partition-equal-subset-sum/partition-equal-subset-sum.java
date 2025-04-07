57ce860c19638da4a85b995908be5362f87b8787
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-04-07 09:35:40 +0530

    Time: 44 ms (48.96%) | Memory: 46.3 MB (67.89%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,74 +1,74 @@
-// bottom up
-// class Solution {
-//     public boolean canPartition(int[] nums) {
-//         int n = nums.length;
-//         int sum = 0;
-//         for (int s : nums)
-//             sum += s;
-
-//         if (sum % 2 != 0)
-//             return false;
-
-//         sum /= 2;
-//         boolean dp[][] = new boolean[n + 1][sum + 1];
-//         //sum == 0
-//         for (int i = 0; i < n + 1; i++) {
-//             dp[i][0] = true;
-//         }
-
-//         for (int i = 1; i < n + 1; i++) {
-//             for (int j = 1; j < sum + 1; j++) {
-//                 if (nums[i - 1] > j) {
-//                     //curr number is bigger than sum
-//                     dp[i][j] = dp[i - 1][j];
-//                 } else {
-//                     //pick, no-pick
-//                     dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
-//                 }
-//             }
-//         }
-//         return dp[n][sum];
-//     }
-// }
-
-
+//bottom up
 class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
+        for (int s : nums)
+            sum += s;
 
-        if (sum % 2 != 0) {
+        if (sum % 2 != 0)
             return false;
-        }
 
         sum /= 2;
-        Boolean[][] memo = new Boolean[n + 1][sum + 1];
-
-        return canPartitionHelper(nums, n, sum, memo);
-    }
-
-    private boolean canPartitionHelper(int[] nums, int index, int target, Boolean[][] memo) {
-        // Base cases
-        if (target == 0) {
-            return true; 
-        }
-        if (index == 0) {
-            return false;
+        boolean dp[][] = new boolean[n + 1][sum + 1];
+        //sum == 0
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = true;
         }
 
-
-        if (memo[index][target] != null) {
-            return memo[index][target];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (nums[i - 1] > j) {
+                    //curr number is bigger than sum
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    //pick, no-pick
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
+                }
+            }
         }
-
-        boolean include = (nums[index - 1] <= target) && canPartitionHelper(nums, index - 1, target - nums[index - 1], memo);
-        boolean exclude = canPartitionHelper(nums, index - 1, target, memo);
-
-        memo[index][target] = include || exclude;
-
-        return memo[index][target];
+        return dp[n][sum];
     }
 }
+
+// top down
+// class Solution {
+//     public boolean canPartition(int[] nums) {
+//         int n = nums.length;
+//         int sum = 0;
+//         for (int num : nums) {
+//             sum += num;
+//         }
+
+//         if (sum % 2 != 0) {
+//             return false;
+//         }
+
+//         sum /= 2;
+//         Boolean[][] memo = new Boolean[n + 1][sum + 1];
+
+//         return canPartitionHelper(nums, n, sum, memo);
+//     }
+
+//     private boolean canPartitionHelper(int[] nums, int index, int target, Boolean[][] memo) {
+//         // Base cases
+//         if (target == 0) {
+//             return true; 
+//         }
+//         if (index == 0) {
+//             return false;
+//         }
+
+
+//         if (memo[index][target] != null) {
+//             return memo[index][target];
+//         }
+
+//         boolean include = (nums[index - 1] <= target) && canPartitionHelper(nums, index - 1, target - nums[index - 1], memo);
+//         boolean exclude = canPartitionHelper(nums, index - 1, target, memo);
+
+//         memo[index][target] = include || exclude;
+
+//         return memo[index][target];
+//     }
+// }
