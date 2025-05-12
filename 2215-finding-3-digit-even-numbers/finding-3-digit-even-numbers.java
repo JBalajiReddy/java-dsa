d46558ca542e28364c773a986a38711a0f77ae6f
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-05-12 09:17:50 +0530

    Time: 5 ms (92.10%) | Memory: 44.7 MB (82.46%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -1,28 +1,67 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        int n = digits.length;
-        Set<Integer> set = new HashSet<>();
-        int num = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j)
-                    continue;
-                for (int k = 0; k < n; k++) {
-                    if (i == k || j == k)
-                        continue;
-                    if (digits[k] % 2 == 0 && digits[i] != 0) {
-                        num = digits[i] * 100 + digits[j] * 10 + digits[k];
-                        set.add(num);
+        int[] count = new int[10];
+        for (int digit : digits) {
+            count[digit]++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 1; i < 10; i++) {
+            if (count[i] > 0) {
+                count[i]--; // Use this digit
+                for (int j = 0; j < 10; j++) {
+                    if (count[j] > 0) {
+                        count[j]--; // Use this digit
+                        for (int k = 0; k < 10; k += 2) {
+                            if (count[k] > 0) {
+
+                                int num = i * 100 + j * 10 + k;
+                                result.add(num);
+                            }
+                        }
+                        count[j]++; // Backtrack
                     }
                 }
+                count[i]++; // Backtrack
             }
         }
-        int[] res = new int[set.size()];
-        int i = 0;
-        for (Integer r : set) {
-            res[i++] = r;
+
+        int[] res = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            res[i] = result.get(i);
         }
         Arrays.sort(res);
+
         return res;
     }
 }
+
+// class Solution {
+//     public int[] findEvenNumbers(int[] digits) {
+//         int n = digits.length;
+//         Set<Integer> set = new HashSet<>();
+//         int num = 0;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (i == j)
+//                     continue;
+//                 for (int k = 0; k < n; k++) {
+//                     if (i == k || j == k)
+//                         continue;
+//                     if (digits[k] % 2 == 0 && digits[i] != 0) {
+//                         num = digits[i] * 100 + digits[j] * 10 + digits[k];
+//                         set.add(num);
+//                     }
+//                 }
+//             }
+//         }
+//         int[] res = new int[set.size()];
+//         int i = 0;
+//         for (Integer r : set) {
+//             res[i++] = r;
+//         }
+//         Arrays.sort(res);
+//         return res;
+//     }
+// }
