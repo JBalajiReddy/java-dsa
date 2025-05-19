8bab496591bfc76fcb101ac6a15a0d3ef0ac1a29
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-05-19 11:10:25 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (74.91%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -1,24 +1,37 @@
 class Solution {
     public String triangleType(int[] nums) {
-        Arrays.sort(nums);
-        int a = nums[0];
-        int b = nums[1];
-        int c = nums[2];
-        if (isTriangle(a, b, c)){
-            if (a == b && b == c) {
-                return "equilateral";
-            } else if (a != b && b != c && c != a) {
-                return "scalene";
-            } else {
-                return "isosceles";
-            }
-        }
-        return "none";
-    }
-    private boolean isTriangle(int a, int b, int c) {
-        if (a + b <= c) {
-            return false;
-        }
-        return true;
+        int a = nums[0], b = nums[1], c = nums[2];
+        if (a + b <= c || a + c <= b || b + c <= a)
+            return "none";
+        if (a == b && b == c)
+            return "equilateral";
+        if (a == b || b == c || a == c)
+            return "isosceles";
+        return "scalene";
     }
 }
+
+// class Solution {
+//     public String triangleType(int[] nums) {
+//         Arrays.sort(nums);
+//         int a = nums[0];
+//         int b = nums[1];
+//         int c = nums[2];
+//         if (isTriangle(a, b, c)){
+//             if (a == b && b == c) {
+//                 return "equilateral";
+//             } else if (a != b && b != c && c != a) {
+//                 return "scalene";
+//             } else {
+//                 return "isosceles";
+//             }
+//         }
+//         return "none";
+//     }
+//     private boolean isTriangle(int a, int b, int c) {
+//         if (a + b <= c) {
+//             return false;
+//         }
+//         return true;
+//     }
+// }
