766b54daac2a09e5b1df2503ef4b716bd972f661
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-05-20 11:18:43 +0530

    Time: 3 ms (79.52%) | Memory: 95.9 MB (37.30%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.java
@@ -1,22 +1,19 @@
-//prefix sum
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
         int n = nums.length;
-        int[] diff = new int[n + 1];
-        for (int query[] : queries) {
-            int l = query[0];
-            int r = query[1];
+        int diff[] = new int[n + 1];
+        for (int q[] : queries) {
+            int l = q[0];
+            int r = q[1];
             diff[l]--;
             if (r + 1 < n)
                 diff[r + 1]++;
         }
-        int decCount = 0;
+        int diffCt = 0;
         for (int i = 0; i < n; i++) {
-            decCount += diff[i];
-            if (decCount + nums[i] > 0) {
+            diffCt += diff[i];
+            if (diffCt + nums[i] > 0)
                 return false;
-                //number cannot be reduced to zero 
-            }
         }
         return true;
     }
