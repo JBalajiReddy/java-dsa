6563d503a896d7fc0184255d4c160195dcbc7788
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-05-24 10:16:40 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (76.79%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.java
+++ 3194-find-words-containing-character/find-words-containing-character.java
@@ -2,13 +2,26 @@ class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
         List<Integer> res = new ArrayList<>();
         for (int i = 0; i < words.length; i++) {
-            for (char ch : words[i].toCharArray()) {
-                if (ch == x) {
-                    res.add(i);
-                    break;
-                }
+            if (words[i].indexOf(x) != -1) {
+                res.add(i);
             }
         }
         return res;
     }
 }
+
+
+// class Solution {
+//     public List<Integer> findWordsContaining(String[] words, char x) {
+//         List<Integer> res = new ArrayList<>();
+//         for (int i = 0; i < words.length; i++) {
+//             for (char ch : words[i].toCharArray()) {
+//                 if (ch == x) {
+//                     res.add(i);
+//                     break;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
