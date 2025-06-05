80103443f1226853448512b7590430edff790624
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-06-05 19:11:50 +0530

    Time: 20 ms (46.70%) | Memory: 45.4 MB (43.33%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,7 +1,6 @@
 class Solution {
     public String minWindow(String s, String t) {
-
-        if (t.length() > s.length())
+        if (s.length() < t.length())
             return "";
         if (s == t)
             return s;
@@ -11,44 +10,36 @@ class Solution {
             totalCts.put(ch, totalCts.getOrDefault(ch, 0) + 1);
         }
 
-        int required = totalCts.size(); 
-        int formed = 0; 
-        Map<Character, Integer> currentCts = new HashMap<>();
-
-        int left = 0, right = 0; 
-        int minLength = Integer.MAX_VALUE; 
-        int minLeft = 0; 
+        int req = totalCts.size();
+        int formed = 0;
+        int left = 0, right = 0;
+        int minLeft = 0, minLen = Integer.MAX_VALUE;
+        Map<Character, Integer> currCts = new HashMap<>();
 
         while (right < s.length()) {
-            char currentChar = s.charAt(right);
-            currentCts.put(currentChar, currentCts.getOrDefault(currentChar, 0) + 1);
-
-            if (totalCts.containsKey(currentChar) &&
-                    currentCts.get(currentChar).intValue() == totalCts.get(currentChar).intValue()) {
+            char currChar = s.charAt(right);
+            currCts.put(currChar, currCts.getOrDefault(currChar, 0) + 1);
+            if (totalCts.containsKey(currChar) &&
+                    currCts.get(currChar).intValue() == totalCts.get(currChar).intValue()) {
                 formed++;
             }
-       
-            //finding min window
-            while (left <= right && formed == required) {
+            while (left <= right && formed == req) {
                 char leftChar = s.charAt(left);
-
-                if (right - left + 1 < minLength) {
-                    minLength = right - left + 1;
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
                     minLeft = left;
                 }
-
-                currentCts.put(leftChar, currentCts.get(leftChar) - 1);
+                currCts.put(leftChar, currCts.get(leftChar) - 1);
                 if (totalCts.containsKey(leftChar) &&
-                        currentCts.get(leftChar).intValue() < totalCts.get(leftChar).intValue()) {
+                        currCts.get(leftChar).intValue() < totalCts.get(leftChar).intValue()) {
                     formed--;
                 }
 
-                left++; 
+                left++;
             }
 
-            right++; 
+            right++;
         }
-
-        return minLength == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLength);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLen);
     }
 }
