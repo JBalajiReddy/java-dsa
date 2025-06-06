cba88ec1b85b7415ef4d1a22680425ce4f8370b5
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-06-06 15:07:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (7.93%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,23 +1,23 @@
-// class Solution {
-//     public void rotate(int[][] matrix) {
-//         int n = matrix.length;
-//         for (int i = 0; i < (n + 1) / 2; i++) {
-//             for (int j = 0; j < n / 2; j++) {
-//                 //4-way swap
-//                 //bottom left
-//                 int temp = matrix[n - 1 - j][i];
-//                 //bottom left <- bottom right
-//                 matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
-//                 //bottom right <- top right
-//                 matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
-//                 //top right <- top left
-//                 matrix[j][n - 1 - i] = matrix[i][j];
-//                 //top left <- temp
-//                 matrix[i][j] = temp;
-//             }
-//         }
-//     }
-// }
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                //4-way swap
+                //bottom left
+                int temp = matrix[n - 1 - j][i];
+                //bottom left <- bottom right
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                //bottom right <- top right
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                //top right <- top left
+                matrix[j][n - 1 - i] = matrix[i][j];
+                //top left <- temp
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
 
 
 
@@ -25,24 +25,24 @@
 // 180 = reverse row + reverse column
 // 270 = transpose + reverse col
 
-class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        // Transpose the matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
-        // Reverse each row
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n / 2; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - 1 - j];
-                matrix[i][n - 1 - j] = temp;
-            }
-        }
-    }
-}
+// class Solution {
+//     public void rotate(int[][] matrix) {
+//         int n = matrix.length;
+//         // Transpose the matrix
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 int temp = matrix[i][j];
+//                 matrix[i][j] = matrix[j][i];
+//                 matrix[j][i] = temp;
+//             }
+//         }
+//         // Reverse each row
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n / 2; j++) {
+//                 int temp = matrix[i][j];
+//                 matrix[i][j] = matrix[i][n - 1 - j];
+//                 matrix[i][n - 1 - j] = temp;
+//             }
+//         }
+//     }
+// }
