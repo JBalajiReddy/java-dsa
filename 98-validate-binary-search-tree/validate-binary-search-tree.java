9ddd067f73e34e4a625aaa65fa9774317596de64
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-06-17 13:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (68.25%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,28 +13,56 @@
  *     }
  * }
  */
+
+
 class Solution {
-    ArrayList<Integer> ls;
+    private Integer prev = null; 
 
     public boolean isValidBST(TreeNode root) {
-        ls = new ArrayList<>();
-        inOrder(root);
-        return isSorted(ls);
+        return inOrder(root);
     }
 
-    private void inOrder(TreeNode root) {
-        if (root == null)
-            return;
-        inOrder(root.left);
-        ls.add(root.val);
-        inOrder(root.right);
-    }
-
-    private boolean isSorted(ArrayList<Integer> ls) {
-        for (int i = 0; i < ls.size() - 1; i++) {
-            if (ls.get(i) >= ls.get(i + 1))
-                return false;
+    private boolean inOrder(TreeNode root) {
+        if (root == null) {
+            return true; 
         }
-        return true;
+
+        if (!inOrder(root.left)) {
+            return false; 
+        }
+
+        if (prev != null && root.val <= prev) {
+            return false; 
+        }
+        prev = root.val; 
+
+        return inOrder(root.right);
     }
 }
+
+
+// class Solution {
+//     ArrayList<Integer> ls;
+
+//     public boolean isValidBST(TreeNode root) {
+//         ls = new ArrayList<>();
+//         inOrder(root);
+//         return isSorted(ls);
+//     }
+
+//     private void inOrder(TreeNode root) {
+//         if (root == null)
+//             return;
+//         inOrder(root.left);
+//         ls.add(root.val);
+//         inOrder(root.right);
+//     }
+
+//     private boolean isSorted(ArrayList<Integer> ls) {
+//         for (int i = 0; i < ls.size() - 1; i++) {
+//             if (ls.get(i) >= ls.get(i + 1))
+//                 return false;
+//         }
+//         return true;
+//     }
+// }
