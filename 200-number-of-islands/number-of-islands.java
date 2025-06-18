af9b149603734023027dbe3ff9294d135eaba51d
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-06-18 11:30:46 +0530

    Time: 3 ms (84.48%) | Memory: 49.7 MB (53.24%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,11 +1,12 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
         int count = 0;
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1') {
-                    recIslands(grid, i, j);
+                    recur(grid, i, j);
                     count++;
                 }
             }
@@ -13,23 +14,14 @@ class Solution {
         return count;
     }
 
-    public void recIslands(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] != '1') {
-            return;
-        }
-
+    private void recur(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') return;
+        
         grid[i][j] = '0';
 
-        recIslands(grid, i + 1, j);
-        recIslands(grid, i, j + 1);
-        recIslands(grid, i - 1, j);
-        recIslands(grid, i, j - 1);
-
-        // int dx[] = {-1, 0, 1, 0};
-        // int dy[] = {0, -1, 0, 1};
-        // for(int idx = 0; idx < 4; idx++){
-        // recIslands(grid, i + dx[idx], j + dy[idx]);
-        // }
-
+        recur(grid, i + 1, j);
+        recur(grid, i, j + 1);
+        recur(grid, i - 1, j);
+        recur(grid, i, j - 1);
     }
 }
