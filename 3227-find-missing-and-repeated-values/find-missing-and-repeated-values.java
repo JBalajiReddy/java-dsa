fb5d1d9867e09a2929c706487282505dfde1c197
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-06-24 12:34:40 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (14.97%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,30 +1,25 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int n = grid.length;
-       // Map<Integer, Integer> mp = new HashMap<>();
-       int[] freq = new int[(n * n) + 1];
-        int gSum = 0;
-        for (int[] g : grid) {
-            for (int i = 0; i < g.length; i++) {
-                // mp.put(num, mp.getOrDefault(num, 0) + 1);
-                freq[g[i]]++;
-                gSum += g[i];
-            }
-        }
-       int repeatedVal = getMaxFreq(freq);
-       int actualSum = (n * n * (n * n + 1)) / 2;
-       int missingVal = actualSum - (gSum - repeatedVal);
-       return new int[]{repeatedVal, missingVal};
-    }
+        int size = n * n;
+        int[] count = new int[size + 1];
 
-    private int getMaxFreq(int[] freq) {
-        int n = 0; 
-        for (int i = 0; i < freq.length; i++) {
-            if (freq[i] == 2 || freq[i] > 1) { 
-                n = i;
-                break;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                count[grid[i][j]]++;
             }
         }
-        return n;
+
+        int a = -1, b = -1;
+
+        for (int num = 1; num <= size; num++) {
+            if (count[num] == 2) {
+                a = num;
+            } else if (count[num] == 0) {
+                b = num;
+            }
+        }
+
+        return new int[] { a, b };
     }
 }
