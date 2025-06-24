7c8a4c289d693618d0f0633a1e6a6a8b6676c454
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-06-24 16:34:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (20.35%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n < 0) {
+        double ans = 1.0;
+        long nn = n; // Convert to long to handle Integer.MIN_VALUE
+
+        if (nn < 0) {
+            nn = -nn;
             x = 1 / x;
-            n = -n;
-            if (n == Integer.MIN_VALUE) {
-                n = Integer.MAX_VALUE;
-                return x * myPow(x, n);
+        }
+
+        while (nn > 0) {
+            if (nn % 2 == 1) {
+                ans = ans * x;
+                nn = nn - 1; 
+            } else {
+                x = x * x;
+                nn = nn / 2;
             }
         }
-        double res = 1;
-        while (n != 0) {
-            if ((n & 1) == 1)
-                res *= x;
-            x *= x;
-            n >>= 1;
-        }
-        return res;
+
+        return ans;
     }
 }
