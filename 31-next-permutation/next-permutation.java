1fbefd4e9abadca62c50a4fb76b35c98728b8f20
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-06-25 17:20:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (36.78%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,26 +1,26 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
-        int j = nums.length - 1; //i + 1
+        int j = nums.length - 1;
 
         while (i >= 0 && nums[i] >= nums[i + 1])
             i--;
         if (i >= 0) {
             while (nums[j] <= nums[i])
-                j--;
+                j--; //find j-th idx from right that's greater than i-th element
             swap(nums, i, j);
         }
         reverse(nums, i + 1, nums.length - 1);
     }
 
-    public void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    public void swap(int[] n, int i, int j) {
+        int temp = n[i];
+        n[i] = n[j];
+        n[j] = temp;
     }
 
-    public void reverse(int[] nums, int i, int j) {
+    public void reverse(int[] n, int i, int j) {
         while (i < j)
-            swap(nums, i++, j--);
+            swap(n, i++, j--);
     }
 }
