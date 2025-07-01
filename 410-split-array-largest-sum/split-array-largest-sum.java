1892b205f40c42991a9bda2ddecbef5630b23bfe
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-01 16:32:01 +0530

    Time: 2 ms (34.13%) | Memory: 41.4 MB (42.73%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -5,6 +5,9 @@ class Solution {
         while (st <= end) {
             int mid = st + (end - st) / 2;
             int count = countSubarraysWithMaxSumLimit(nums, mid);
+            //Check: Can we split the array into ≤ k subarrays such that no subarray sum exceeds mid?
+            //If yes → try smaller max
+            //If no → we need a bigger max sum
             if (count > k)
                 st = mid + 1;
             else
