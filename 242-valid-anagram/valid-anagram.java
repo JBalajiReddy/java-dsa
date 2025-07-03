aa9e782ce91cdd051a8392e57c2aa75510424968
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-03 13:09:53 +0530

    Time: 4 ms (77.58%) | Memory: 43.6 MB (60.88%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,29 @@
-class Solution {
+public class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] c1 = s.toCharArray();
-        Arrays.sort(c1);
-        char[] c2 = t.toCharArray();
-        Arrays.sort(c2);
-
-        return new String(c1).equals(new String(c2));
+        if (s.length() != t.length())
+            return false;
+        int[] a = new int[26];
+        for (Character c : s.toCharArray())
+            a[c - 'a']++;
+        for (Character c : t.toCharArray()) {
+            if (a[c - 'a'] == 0)
+                return false;
+            a[c - 'a']--;
+        }
+        return true;
     }
 }
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         char[] c1 = s.toCharArray();
+//         Arrays.sort(c1);
+//         char[] c2 = t.toCharArray();
+//         Arrays.sort(c2);
+
+//         return new String(c1).equals(new String(c2));
+//     }
+// }
 
 // == compares references, not content, when used with objects (like String)
 
