54dad45951217d915dea42de1820371b5504ba78
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-15 21:31:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (22.25%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -10,28 +10,31 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        int cnt = 0;
-        ListNode tmp = head;
-        //check if k nodes exist
-        while (cnt < k) {
-            if (tmp == null) return head;
-            tmp = tmp.next;
-            cnt++;
+        // Step 1: Check if there are at least k nodes to reverse
+        int count = 0;
+        ListNode current = head;
+        while (count < k) {
+            if (current == null) return head; 
+            current = current.next;
+            count++;
         }
-        //recursive call for rest of LL
-        ListNode prevNode = reverseKGroup(tmp, k);
 
-        //reverse current group
-        tmp = head;
-        cnt = 0;
-        while (cnt < k) {
-            ListNode next = tmp.next;
-            tmp.next = prevNode;
-            prevNode = tmp;
-            tmp = next;
+        // Step 2: Recursively reverse the rest of the list beyond this k-group
+        ListNode reversedRest = reverseKGroup(current, k);
 
-            cnt++;
+        // Step 3: Reverse the current group of k nodes
+        current = head;
+        count = 0;
+
+        while (count < k) {
+            ListNode nextNode = current.next;   // Save next node
+            current.next = reversedRest;        // Reverse pointer to previously reversed part
+            reversedRest = current;             // Move reversedRest to current node
+            current = nextNode;                 // Advance to next node
+            count++;
         }
-        return prevNode;
+
+        // Step 4: Return new head of this reversed group
+        return reversedRest;
     }
 }
