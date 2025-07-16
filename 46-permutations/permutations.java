2bfc991aeb21b3733cc1aef329f65c379731352a
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-16 23:58:28 +0530

    Time: 2 ms (37.03%) | Memory: 44.7 MB (44.91%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,32 +1,54 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> ls = new ArrayList<>();
-        backtrack(0, nums.length, ls, res, nums);
-        return res;
+        List<List<Integer>> list = new ArrayList<>();
+        // Arrays.sort(nums); // not necessary
+        backtrack(list, new ArrayList<>(), nums);
+        return list;
     }
 
-    private void backtrack(int i, int n, List<Integer> ls, List<List<Integer>> res, int[] nums) {
-        if (i == n) {
-            res.add(new ArrayList<>(ls));
-            return;
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            list.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (tempList.contains(nums[i]))
+                    continue; // element already exists, skip
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
         }
-
-        for (int idx = i; idx < n; idx++) {
-            swap(nums, i, idx); // Swap the current index with idx
-            ls.add(nums[i]); 
-            backtrack(i + 1, n, ls, res, nums); // Recurse for the next index
-
-
-            ls.remove(ls.size() - 1); // Backtrack: remove the last added number
-            swap(nums, i, idx); // Swap back to restore the original array
-        }
-
-    }
-
-    private void swap(int[] nums, int i, int idx) {
-        int tmp = nums[i];
-        nums[i] = nums[idx];
-        nums[idx] = tmp;
     }
 }
+
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         List<Integer> ls = new ArrayList<>();
+//         backtrack(0, nums.length, ls, res, nums);
+//         return res;
+//     }
+
+//     private void backtrack(int i, int n, List<Integer> ls, List<List<Integer>> res, int[] nums) {
+//         if (i == n) {
+//             res.add(new ArrayList<>(ls));
+//             return;
+//         }
+
+//         for (int idx = i; idx < n; idx++) {
+//             swap(nums, i, idx); // Swap the current index with idx
+//             ls.add(nums[i]); 
+//             backtrack(i + 1, n, ls, res, nums); // Recurse for the next index
+
+//             ls.remove(ls.size() - 1); // Backtrack: remove the last added number
+//             swap(nums, i, idx); // Swap back to restore the original array
+//         }
+
+//     }
+
+//     private void swap(int[] nums, int i, int idx) {
+//         int tmp = nums[i];
+//         nums[i] = nums[idx];
+//         nums[idx] = tmp;
+//     }
+// }
