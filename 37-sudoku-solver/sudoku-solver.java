903bd84b51efcd671120b03753cf0807842f64aa
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-21 22:59:50 +0530

    Time: 165 ms (83.32%) | Memory: 41.7 MB (8.30%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,59 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board, 0, 0);
+    }
+
+    private boolean solve(char[][] b, int r, int c) {
+        if (r == 9)
+            return true;
+
+        int nxRow = r;
+        int nxCol = c + 1;
+        if (nxCol == 9) {
+            nxRow = r + 1;
+            nxCol = 0;
+        }
+
+        //its a number (filled)
+        if (b[r][c] != '.') {
+            return solve(b, nxRow, nxCol);
+        }
+
+        //place digit
+        for (char d = '1'; d <= '9'; d++) {
+            if (isSafe(b, r, c, d)) {
+                b[r][c] = d;
+                if (solve(b, nxRow, nxCol))
+                    return true;
+
+                b[r][c] = '.';
+            }
+        }
+        return false;
+    }
+
+    private boolean isSafe(char[][] b, int r, int c, char d) {
+        int num = d - '0';
+
+        //row or horizantal
+        for (int col = 0; col < 9; col++) {
+            if (b[r][col] == d) return false;
+        }
+
+        //col or vertical
+        for (int row = 0; row < 9; row++) {
+            if (b[row][c] == d) return false;
+        }
+
+        //3X3
+        int cellRow = 3 * (r / 3);
+        int cellCol = 3 * (c / 3);
+        for (int i = cellRow; i < cellRow + 3; i++) {
+            for (int j = cellCol; j < cellCol + 3; j++) {
+                if (b[i][j] == d) return false;
+            }
+        }
+
+        return true;
+    }
+}
