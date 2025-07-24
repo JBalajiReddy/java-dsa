bb8c5199f62e8e7d4d8c2068c5996dadfa3758c1
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-24 23:19:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (60.43%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,46 +1,39 @@
-//bottom up
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m + 1][n + 1];
-        for (int i = 0; i < m + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                if (i == 0 || j == 0) {  //out of bound cells
-                    dp[i][j] = 0;
-                } else if (i == 1 && j == 1) {   //1 way to reach starting cells
-                    dp[i][j] = 1;
-                } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        int[] prev = new int[n];
+        
+        for (int i = 0; i < m; i++) {
+            int[] curr = new int[n];
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) curr[j] = 1;
+                else {
+                    int down = 0;
+                    int right = 0;
+                    if (i > 0) down = prev[j];
+                    if (j > 0) right = curr[j - 1];
+                    curr[j] = down + right;
                 }
             }
+            prev = curr;
         }
-        return dp[m][n];
+        return prev[n - 1];
     }
 }
 
-
-
-// top down
 // class Solution {
 //     public int uniquePaths(int m, int n) {
-//         int dp[][] = new int[m + 1][n + 1];
-//         for (int i = 0; i < m + 1; i++) {
-//             Arrays.fill(dp[i], -1);
+//         int[][] dp = new int[m + 1][n + 1];
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (i == 0 && j == 0) dp[i][j] = 1;
+//                 else {
+//                     int left = 0, up = 0;
+//                     if (i > 0) up = dp[i - 1][j];
+//                     if (j > 0) left = dp[i][j - 1];
+//                     dp[i][j] = left + up;
+//                 }
+//             }
 //         }
-//         return topDownDP(m, n, dp);
-//     }
-
-//     private int topDownDP(int m, int n, int[][] dp) {
-//         if (m == 0 || n == 0) {
-//             dp[m][n] = 0;
-//             return 0;
-//         } else if (m == 1 && n == 1) {
-//             dp[m][n] = 1;
-//             return 1;
-//         }
-//         if (dp[m][n] != -1) {
-//             return dp[m][n];
-//         }
-//         dp[m][n] = topDownDP(m - 1, n, dp) + topDownDP(m, n - 1, dp);
-//         return dp[m][n];
+//         return dp[m - 1][n - 1];
 //     }
 // }
