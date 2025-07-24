8d599a6efd6646350bf8643da04f04f35de0e695
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-24 07:41:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (14.95%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,3 +1,5 @@
+//BOTTOM UP
+
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
@@ -31,5 +33,29 @@ class Solution {
 //             dp[i] = Math.max(pick, noPick);
 //         }
 //         return dp[n - 1];
+//     }
+// }
+
+
+//TOP - DOWN
+
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n + 1];
+//         Arrays.fill(dp, - 1);
+//         return solve(dp, nums, n - 1);
+//     }
+
+//     private int solve(int[] dp, int[] n, int i) {
+//         if (i == 0) return n[i];
+//         if (i < 0) return 0;
+//         if (dp[i] != -1) return dp[i];
+
+//         int pick = n[i] + solve(dp, n, i - 2);
+//         int noPick = solve(dp, n, i - 1);
+//         dp[i] = Math.max(pick, noPick);
+
+//         return dp[i];
 //     }
 // }
