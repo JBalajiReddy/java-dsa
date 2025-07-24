48fdf17ec735c104b7960f585763cd5bf02f9c59
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-24 07:21:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (98.24%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,56 +1,35 @@
-//recursive + memo (top - down)
-// class Solution {
-//     int[] memo;
-//     public int rob(int[] nums) {
-//         //skip : robHouse(i - 1)
-//         //rob : robHouse(i - 2) + nums[i]
-//         memo = new int[nums.length + 1];
-//         Arrays.fill(memo, -1);
-//         return robHouse(nums, nums.length - 1);
-//     }
-//     public int robHouse(int[] nums, int i){
-//         if(i < 0)
-//            return 0;
-//         if(memo[i] >= 0)
-//             return memo[i];
-//         int res =  Math.max(robHouse(nums, i - 2) + nums[i], robHouse(nums, i - 1));
-//         memo[i] = res;
-//         return res;
-//     }
-// }
-
-//bottom-up (tabulation)
-// class Solution {
-//     public int rob (int[] nums) {
-//         int n = nums.length;
-//         int[] dp = new int[n + 1];
-//         dp[0] = 0;
-//         dp[1] = nums[0];
-//         for (int i = 2; i < n + 1; i++) {
-//             int rob = nums[i - 1] + dp[i - 2];
-//             int skip = dp[i - 1];
-//             dp[i] = Math.max(rob, skip);
-//         }
-//         return dp[n];
-//     }
-// }
-
-
-//without shifting (bottom-up)
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-        
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        int pre1 = nums[0];
+        int pre2 = 0;
+        for (int i = 1; i < n; i++) {
+            int pick = nums[i] + (i > 1 ? pre2 : 0);
+
+            int noPick = pre1;
+            int curr = Math.max(pick, noPick);
+
+            pre2 = pre1;
+            pre1 = curr;
         }
-        
-        return dp[n - 1];
+        return pre1;
     }
 }
+
+
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n + 1];
+//         dp[0] = nums[0];
+//         for (int i = 1; i < n; i++) {
+//             int pick = nums[i] + (i > 1 ? dp[i - 2] : 0);
+//             // if (i > 1)
+//             //     pick += dp[i - 2];
+
+//             int noPick = dp[i - 1];
+//             dp[i] = Math.max(pick, noPick);
+//         }
+//         return dp[n - 1];
+//     }
+// }
