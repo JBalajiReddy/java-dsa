a8ff04c5cf1e8cb4c4cebe7e3e0154002769c4c4
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-25 06:37:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (38.04%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,46 +1,27 @@
-// bottom up
-// class Solution {
-//     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-//         int m = obstacleGrid.length;
-//         int n = obstacleGrid[0].length;
-//         int dp[][] = new int[m + 1][n + 1];
-//         for (int i = 0; i < m + 1; i++) {
-//             for (int j = 0; j < n + 1; j++) {
-//                 if (i == 0 || j == 0 || obstacleGrid[i - 1][j - 1] == 1)
-//                     dp[i][j] = 0;
-//                 else if (i == 1 && j == 1)
-//                     dp[i][j] = 1;
-//                 else
-//                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-//             }
-//         }
-//         return dp[m][n];
-//     }
-// }
-
-// top down
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        int dp[][] = new int[m + 1][n + 1];
-        for (int i = 0; i < m + 1; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return topDownDP(obstacleGrid, m, n, dp);
+        int m = obstacleGrid.length;
+        int[][] dp = new int[m][n];
+        for (int[] r : dp)
+            Arrays.fill(r, -1);
+        return solve(obstacleGrid, m - 1, n - 1, dp);
     }
-    private int topDownDP(int[][] obstacleGrid, int m, int n, int[][] dp) {
-        if (m == 0 || n == 0 || obstacleGrid[m - 1][n - 1] == 1) {
-            dp[m][n] = 0;
+
+    private int solve(int[][] grid, int m, int n, int[][] dp) {
+        if (m < 0 || n < 0 || grid[m][n] == 1)
             return 0;
-        } else if (m == 1 && n == 1) {
+
+        if (m == 0 && n == 0) {
             dp[m][n] = 1;
             return 1;
-        } else if (dp[m][n] != -1) {
-            return dp[m][n];
-        } else {
-            dp[m][n] = topDownDP(obstacleGrid, m - 1, n, dp) + topDownDP(obstacleGrid, m, n - 1, dp);;
         }
+
+        if (dp[m][n] != -1)
+            return dp[m][n];
+
+        dp[m][n] = solve(grid, m - 1, n, dp) + solve(grid, m, n - 1, dp);
+
         return dp[m][n];
     }
 }
