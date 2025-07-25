333a8df73333152b7f96d616e99903235faf2744
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-25 15:25:34 +0530

    Time: 5 ms (27.61%) | Memory: 47.4 MB (89.76%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,28 +3,58 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] dp = new int[m][n];
+        int[] prev = new int[n];
 
         for (int i = 0; i < m; i++) {
+            int[] curr = new int[n];
             for (int j = 0; j < n; j++) {
                 if (i == 0 && j == 0)
-                    dp[i][j] = grid[i][j];
+                    curr[j] = grid[i][j];
                 else {
                     int up = grid[i][j], left = grid[i][j];
                     if (i > 0)
-                        up += dp[i - 1][j];
+                        up += prev[j];
                     else up+= (int) Math.pow(10, 9);
                     if (j > 0)
-                        left += dp[i][j - 1];
+                        left += curr[j - 1];
                     else
                         left += (int) Math.pow(10, 9);
-                    dp[i][j] = Math.min(up, left);
+                    curr[j] = Math.min(up, left);
                 }
             }
+            prev = curr;
         }
-        return dp[m - 1][n - 1];
+        return prev[n - 1];
     }
 }
+
+//BOTTOM UP
+// class Solution {
+//     public int minPathSum(int[][] grid) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         int[][] dp = new int[m][n];
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (i == 0 && j == 0)
+//                     dp[i][j] = grid[i][j];
+//                 else {
+//                     int up = grid[i][j], left = grid[i][j];
+//                     if (i > 0)
+//                         up += dp[i - 1][j];
+//                     else up+= (int) Math.pow(10, 9);
+//                     if (j > 0)
+//                         left += dp[i][j - 1];
+//                     else
+//                         left += (int) Math.pow(10, 9);
+//                     dp[i][j] = Math.min(up, left);
+//                 }
+//             }
+//         }
+//         return dp[m - 1][n - 1];
+//     }
+// }
 
 //top-down
 // class Solution {
