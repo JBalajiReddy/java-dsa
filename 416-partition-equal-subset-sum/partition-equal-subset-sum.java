018bdff73dbbd9bb509b65396ecb7e9ac2448a7f
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-27 13:35:23 +0530

    Time: 96 ms (19.04%) | Memory: 56.8 MB (39.74%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,7 +1,5 @@
-//bottom up
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
         int sum = 0;
         for (int s : nums)
             sum += s;
@@ -9,66 +7,31 @@ class Solution {
         if (sum % 2 != 0)
             return false;
 
-        sum /= 2;
-        boolean dp[][] = new boolean[n + 1][sum + 1];
-        //sum == 0
-        for (int i = 0; i < n + 1; i++) {
-            dp[i][0] = true;
-        }
+        sum = sum / 2;
+        int n = nums.length;
+        int[][] dp = new int[n + 1][sum + 1];
+        for (int[] r : dp)
+            Arrays.fill(r, -1);
+        return solve(nums, n - 1, sum, dp);
+    }
 
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < sum + 1; j++) {
-                if (nums[i - 1] > j) {
-                    //curr number is bigger than sum
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    //pick, no-pick
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
-                }
-            }
-        }
-        return dp[n][sum];
+    private boolean solve(int[] nums, int i, int t, int[][] dp) {
+        if (t == 0)
+            return true;
+        if (i == 0)
+            return nums[i] == t;
+        if (dp[i][t] != -1)
+            return dp[i][t] == 1;
+
+        //dp[i][t] == 1 means subset is possible (i.e., return true)
+        //dp[i][t] == 0 means subset is not possible (i.e., return false)
+
+        boolean pick = false;
+        boolean noPick = solve(nums, i - 1, t, dp);
+        if (t >= nums[i])
+            pick = solve(nums, i - 1, t - nums[i], dp);
+
+        dp[i][t] = (pick || noPick) ? 1 : 0;
+        return pick || noPick;
     }
 }
-
-// top down
-// class Solution {
-//     public boolean canPartition(int[] nums) {
-//         int n = nums.length;
-//         int sum = 0;
-//         for (int num : nums) {
-//             sum += num;
-//         }
-
-//         if (sum % 2 != 0) {
-//             return false;
-//         }
-
-//         sum /= 2;
-//         Boolean[][] memo = new Boolean[n + 1][sum + 1];
-
-//         return canPartitionHelper(nums, n, sum, memo);
-//     }
-
-//     private boolean canPartitionHelper(int[] nums, int index, int target, Boolean[][] memo) {
-//         // Base cases
-//         if (target == 0) {
-//             return true; 
-//         }
-//         if (index == 0) {
-//             return false;
-//         }
-
-
-//         if (memo[index][target] != null) {
-//             return memo[index][target];
-//         }
-
-//         boolean include = (nums[index - 1] <= target) && canPartitionHelper(nums, index - 1, target - nums[index - 1], memo);
-//         boolean exclude = canPartitionHelper(nums, index - 1, target, memo);
-
-//         memo[index][target] = include || exclude;
-
-//         return memo[index][target];
-//     }
-// }
