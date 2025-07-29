0af5bc3cc21632efad163ee116698415df557c63
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-29 07:11:58 +0530

    Time: 7 ms (72.12%) | Memory: 41.5 MB (78.27%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,42 +1,53 @@
+//BOTTOM-UP
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int dp[][] = new int[n][amount + 1];
+        int[] prev = new int[amount + 1];
+        int[] curr = new int[amount + 1];
 
-        //index == 0
-        for (int a = 0; a < amount + 1; a++) {
-            if (a % coins[0] == 0) {
-                dp[0][a] = 1;
-            } else {
-                dp[0][a] = 0;
-            }
+        for (int a = 0; a <= amount; a++) { //i == 0
+            if (a % coins[0] == 0)
+                prev[a] = 1;
         }
+
+        for (int i = 0; i < n; i++)      //amount == 0
+            curr[0] = 1;
 
         for (int i = 1; i < n; i++) {
-            for (int a = 0; a < amount + 1; a++) {
+            for (int a = 1; a <= amount; a++) {
                 int pick = 0;
-                if (a >= coins[i]) {
-                    pick = dp[i][a - coins[i]];
-                }
-                int noPick = dp[i - 1][a];
-                dp[i][a] = pick + noPick;
+                int noPick = prev[a];
+                if (coins[i] <= a)
+                    pick = curr[a - coins[i]];
+
+                curr[a] = pick + noPick;
             }
+            prev = curr;
         }
-        return dp[n - 1][amount];
+
+        return prev[amount];
     }
 }
 
-
-
+//TOP-DOWN - [TLE]
 // class Solution {
 //     public int change(int amount, int[] coins) {
-//         int[] dp = new int[amount+1];
-//         dp[0] = 1;
-//         for(int c : coins) {
-//             for(int a = c; a <= amount; a++) {
-//                 dp[a] += dp[a-c];
-//             }
-//         }
-//         return dp[amount];
+//         int n = coins.length;
+//         int[][] dp = new int[n][amount + 1];
+//         for (int[] r : dp) Arrays.fill(r, -1);
+
+//         return solve(n - 1, amount, coins, dp);
+//     }
+
+//     private int solve(int i, int t, int[] coins, int[][] dp) {
+//         if (i == 0) return (t % coins[0] == 0) ? 1 : 0;
+
+//         if (t == 0) return 1;
+
+//         int pick = 0;
+//         int noPick = solve(i - 1, t, coins, dp);
+//         if (coins[i] <= t) pick = solve(i, t - coins[i], coins, dp);
+
+//         return dp[i][t] = pick + noPick; 
 //     }
 // }
