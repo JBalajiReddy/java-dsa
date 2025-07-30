89ed24d6a9c227e10d113af765c804a3528d1a11
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-30 22:37:09 +0530

    Time: 1 ms (99.48%) | Memory: 44.9 MB (86.02%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,22 +1,41 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n]; //reach n - 1
+        int left = 0, right = 0;
+        int jumps = 0;
 
-        for (int i = 1; i < n; i++)
-            dp[i] = Integer.MAX_VALUE; //since we are standing in the first position, it is reachable by default without making any move
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j <= i + nums[i] && j < n; j++) { 
-                
-                //From index i, you can jump to index j (where j is between i and i + nums[i])
-                //We update dp[j] with the minimum jumps needed to reach j, which is either:
-                //the previous value of dp[j]
-                //or dp[i] + 1 (one jump from i)
-
-                dp[j] = Math.min(dp[j], dp[i] + 1);
+        while (right < n - 1) {
+            int farthest = 0;
+            for (int i = left; i <= right; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
             }
+            left = right + 1;
+            right = farthest;
+            jumps++;
         }
-        return dp[n - 1];
+        return jumps;
     }
 }
+
+// class Solution {
+//     public int jump(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n]; //reach n - 1
+
+//         for (int i = 1; i < n; i++)
+//             dp[i] = Integer.MAX_VALUE; //since we are standing in the first position, it is reachable by default without making any move
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i; j <= i + nums[i] && j < n; j++) { 
+
+//                 //From index i, you can jump to index j (where j is between i and i + nums[i])
+//                 //We update dp[j] with the minimum jumps needed to reach j, which is either:
+//                 //the previous value of dp[j]
+//                 //or dp[i] + 1 (one jump from i)
+
+//                 dp[j] = Math.min(dp[j], dp[i] + 1);
+//             }
+//         }
+//         return dp[n - 1];
+//     }
+// }
