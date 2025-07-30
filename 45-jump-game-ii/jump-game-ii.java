c9ef311399eed38b65da7c86d631c87fc4d94902
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-07-30 21:33:25 +0530

    Time: 39 ms (14.46%) | Memory: 45.8 MB (13.94%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,13 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n];
+        int[] dp = new int[n]; //reach n - 1
 
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+        for (int i = 1; i < n; i++)
+            dp[i] = Integer.MAX_VALUE; //since we are standing in the first position, it is reachable by default without making any move
 
         for (int i = 0; i < n; i++) {
-            for (int j = i; j <= i + nums[i] && j < n; j++) {
+            for (int j = i; j <= i + nums[i] && j < n; j++) { 
+                
+                //From index i, you can jump to index j (where j is between i and i + nums[i])
+                //We update dp[j] with the minimum jumps needed to reach j, which is either:
+                //the previous value of dp[j]
+                //or dp[i] + 1 (one jump from i)
+
                 dp[j] = Math.min(dp[j], dp[i] + 1);
             }
         }
