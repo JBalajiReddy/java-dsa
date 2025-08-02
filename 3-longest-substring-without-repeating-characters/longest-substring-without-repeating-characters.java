22be35581aa8c347e26a518559988def92228047
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-08-02 16:24:25 +0530

    Time: 5 ms (89.10%) | Memory: 44.6 MB (78.88%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -4,13 +4,14 @@ class Solution {
         int left = 0, right = 0;
         int maxLen = 0;
 
-        Map<Character, Integer> mp = new HashMap<>();
+        Map<Character, Integer> mp = new HashMap<>(); // map to store last index of each character
         while (right < n) {
             char ch = s.charAt(right);
+            // if character already seen, move left pointer to avoid duplicate
             if (mp.containsKey(ch))
                 left = Math.max(mp.get(ch) + 1, left);
 
-            mp.put(ch, right);
+            mp.put(ch, right); // update character's latest index
             maxLen = Math.max(maxLen, right - left + 1);
             right++;
         }
