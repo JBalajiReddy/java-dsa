4bd9a2f38996d75506c526965b8f466d7b58a888
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-08-03 12:17:39 +0530

    Time: 55 ms (14.47%) | Memory: 55.4 MB (31.45%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,28 @@
+//Find the longest continuous sub array that has exactly 2 distinct elements.
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        int left = 0, right = 0, maxLen = 0;
+
+        Map<Integer, Integer> mp = new HashMap<>();
+        while (right < n) {
+            mp.put(fruits[right], mp.getOrDefault(fruits[right], 0) + 1);
+
+            if (mp.size() > 2) {
+                while (mp.size() > 2) {
+                    mp.put(fruits[left], mp.getOrDefault(fruits[left], 0) - 1);
+                    if (mp.get(fruits[left]) == 0)
+                        mp.remove(fruits[left]);
+
+                    left++;
+                }
+            }
+
+            if (mp.size() <= 2) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            right++;
+        }
+        return maxLen;
+    }
+}
