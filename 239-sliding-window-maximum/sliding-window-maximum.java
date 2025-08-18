73a766c1ea1f4c003a65a90009616f7cc941611a
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-08-18 08:02:12 +0530

    Time: 30 ms (80.51%) | Memory: 59.6 MB (39.17%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,46 +1,31 @@
+//NGE - monotonic decreasing
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
         if (n == 1 || k == 1)
             return nums;
-        Deque<Integer> dq = new ArrayDeque<>();
+        Deque<Integer> dq = new ArrayDeque<>(); //store index
+        int ri = 0;
         int[] res = new int[n - k + 1];
-        int idx = 0;
 
         for (int i = 0; i < n; i++) {
 
-            if (!dq.isEmpty() && dq.getFirst() <= i - k) // remove out-of-bound indices
+            //remove out-of-bound indices
+            if (!dq.isEmpty() && dq.getFirst() <= i - k)
                 dq.removeFirst();
-            while (!dq.isEmpty() && nums[dq.getLast()] < nums[i])  // Maintain descending order in deque
+
+            // remove smaller numbers in k range as they are useless
+            while (!dq.isEmpty() && nums[dq.getLast()] <= nums[i])
                 dq.removeLast();
-          
+
             dq.addLast(i);
 
             if (i >= k - 1)
-                res[idx++] = nums[dq.getFirst()]; // Start adding to result once the first window is hit
+                res[ri++] = nums[dq.getFirst()];
         }
 
         return res;
     }
 }
 
-// class Solution {
-//     public int[] maxSlidingWindow(int[] nums, int k) {
-//         List<Integer> result = new ArrayList<>();
-//         int n = nums.length;
-
-//         for (int i = 0; i <= n - k; i++) {
-//             int max = nums[i];
-//             for (int j = i + 1; j < i + k; j++) {
-//                 max = Math.max(max, nums[j]);
-//             }
-//             result.add(max);
-//         }
-
-//         int[] res = new int[result.size()];
-//         int idx = 0;
-//         for (int i : result)
-//             res[idx++] = i;
-//         return res;
-//     }
-// }
+//first ------ last
