607beb684686b5b3c8355dbf6ebe3a17e01ac96c
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-09-02 07:58:43 +0530

    Time: 1 ms (99.66%) | Memory: 46.1 MB (9.51%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -2,12 +2,12 @@ class Solution {
     public int[] singleNumber(int[] nums) {
         long xor = 0;
         for (int num : nums) xor ^= num; 
-        long differIdx = (xor & (xor - 1)) ^ xor; //right most differing idx
+        long rightmostSetBit = (xor & (xor - 1)) ^ xor; //right most differing idx
         //two unique numebrs will have this idx different
         int b1 = 0;
         int b2 = 0;
         for (int num : nums) {
-            if ((num & differIdx) != 0)  b1 ^= num;
+            if ((num & rightmostSetBit) != 0)  b1 ^= num;
             else b2 ^= num;
         }
         return new int[]{b1, b2};
