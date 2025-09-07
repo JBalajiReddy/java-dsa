367d1f9ab469f4f850f10acc13ee6b9d1667dde1
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-09-07 16:41:59 +0530

    Time: 6 ms (22.83%) | Memory: 52.2 MB (19.74%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,27 +1,54 @@
 class Solution {
+    class Pair {
+        int r, c;
+
+        Pair(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1') {
-                    recur(grid, i, j);
-                    count++;
+        int[][] visited = new int[n][m];
+        int cnt = 0;
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (visited[row][col] == 0 && grid[row][col] == '1') {
+                    cnt++;
+                    bfs(row, col, visited, grid);
                 }
             }
         }
-        return count;
+        return cnt;
     }
 
-    private void recur(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') return;
-        
-        grid[i][j] = '0';
+    private void bfs(int row, int col, int[][] vis, char[][] grid) {
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(row, col));
+        vis[row][col] = 1;
+        int n = grid.length, m = grid[0].length;
 
-        recur(grid, i + 1, j);
-        recur(grid, i, j + 1);
-        recur(grid, i - 1, j);
-        recur(grid, i, j - 1);
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, 1, 0, -1};
+
+        while (!q.isEmpty()) {
+            Pair pair = q.poll();
+            int ro = pair.r;
+            int co = pair.c;
+
+            for (int i = 0; i < 4; i++) {
+                int nRow = ro + dRow[i];
+                int nCol = co + dCol[i];
+
+                if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && vis[nRow][nCol] == 0
+                        && grid[nRow][nCol] == '1') {
+                    vis[nRow][nCol] = 1;
+                    q.offer(new Pair(nRow, nCol));
+                }
+            }
+        }
     }
 }
