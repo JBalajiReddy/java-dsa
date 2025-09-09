b1eb09a60f90654241ac4683d1a461e9f77f4df9
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-09-09 07:19:05 +0530

    Time: 7 ms (9.83%) | Memory: 45.5 MB (20.27%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,20 +1,16 @@
+import java.util.*;
+
 class Solution {
 
-    private boolean bfs(ArrayList<ArrayList<Integer>> adj, int start, int[] color) {
-        int V = adj.size();
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        color[start] = 0;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-
-            for (Integer neigh : adj.get(node)) {
-                if (color[neigh] == -1) {
-                    q.offer(neigh);
-                    color[neigh] = 1 - color[node];
-                } else if (color[neigh] == color[node])
+    private boolean dfs(ArrayList<ArrayList<Integer>> adj, int node, int[] color, int currColor) {
+        color[node] = currColor;
+        for (int neigh : adj.get(node)) {
+            if (color[neigh] == -1) {
+                if (!dfs(adj, neigh, color, 1 - currColor)) {
                     return false;
+                }
+            } else if (color[neigh] == color[node]) {
+                return false;
             }
         }
         return true;
@@ -23,8 +19,7 @@ class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            adj.add(new ArrayList<>());
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < graph[i].length; j++) {
                 adj.get(i).add(graph[i][j]);
@@ -35,11 +30,60 @@ class Solution {
         Arrays.fill(color, -1);
         for (int i = 0; i < n; i++) {
             if (color[i] == -1) {
-                if (bfs(adj, i, color) == false)
+                if (!dfs(adj, i, color, 0)) {
                     return false;
+                }
             }
         }
-
         return true;
     }
 }
+
+
+
+
+// class Solution {
+
+//     private boolean bfs(ArrayList<ArrayList<Integer>> adj, int start, int[] color) {
+//         int V = adj.size();
+//         Queue<Integer> q = new LinkedList<>();
+//         q.offer(start);
+//         color[start] = 0;
+
+//         while (!q.isEmpty()) {
+//             int node = q.poll();
+
+//             for (Integer neigh : adj.get(node)) {
+//                 if (color[neigh] == -1) {
+//                     q.offer(neigh);
+//                     color[neigh] = 1 - color[node];
+//                 } else if (color[neigh] == color[node])
+//                     return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     public boolean isBipartite(int[][] graph) {
+//         int n = graph.length;
+//         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+//         for (int i = 0; i < n; i++)
+//             adj.add(new ArrayList<>());
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < graph[i].length; j++) {
+//                 adj.get(i).add(graph[i][j]);
+//             }
+//         }
+
+//         int[] color = new int[n];
+//         Arrays.fill(color, -1);
+//         for (int i = 0; i < n; i++) {
+//             if (color[i] == -1) {
+//                 if (bfs(adj, i, color) == false)
+//                     return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// }
