e00b59afaadbcb35b54f6415ed9ecd96e8598ef1
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-09-10 13:59:10 +0530

    Time: 6 ms (72.14%) | Memory: 45.3 MB (68.52%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -4,24 +4,18 @@ class Solution {
         for (int i = 0; i < V; i++)
             adj.add(new ArrayList<>());
 
+        //Apply - topological sort
+        //BFS - kahn's algo
+        int[] inDegree = new int[V];
         for (int p[] : prerequisites) {
             int u = p[0];
             int v = p[1];
             adj.get(u).add(v);
-        }
-
-        //Apply - topological sort
-        //BFS - kahn's algo
-        int[] inDegree = new int[V];
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < adj.get(i).size(); j++) {
-                int n = adj.get(i).get(j);
-                inDegree[n]++;
-            }
+            inDegree[v]++;
         }
 
         int cnt = 0;
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
         for (int i = 0; i < V; i++) {
             if (inDegree[i] == 0) {
                 q.offer(i);
