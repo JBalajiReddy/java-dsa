8600c72ff6d1f6271009fbc5b64a7fcef360b9a6
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-09-11 23:13:43 +0530

    Time: 96 ms (33.96%) | Memory: 46.5 MB (63.38%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -2,39 +2,38 @@ class Solution {
     class Pair {
         String w;
         int lvl;
-
         Pair(String w, int lvl) {
             this.w = w;
             this.lvl = lvl;
         }
     }
-
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
         Queue<Pair> q = new ArrayDeque<>();
         q.offer(new Pair(beginWord, 1));
-        Set<String> set = new HashSet<>(wordList);
         set.remove(beginWord);
 
         while (!q.isEmpty()) {
             Pair p = q.poll();
             String word = p.w;
             int lvl = p.lvl;
-            if (word.equals(endWord))
-                return lvl;
+
+            if (word.equals(endWord)) return lvl;
 
             for (int i = 0; i < word.length(); i++) {
                 for (char ch = 'a'; ch <= 'z'; ch++) {
-                    char[] replacedArr = word.toCharArray();
-                    replacedArr[i] = ch;
-                    String newWord = new String(replacedArr);
+                    char[] replacedWord = word.toCharArray();
+                    replacedWord[i] = ch;
+                    String newWord = new String(replacedWord);
 
                     if (set.contains(newWord)) {
-                        set.remove(newWord);
                         q.offer(new Pair(newWord, lvl + 1));
+                        set.remove(newWord);
                     }
                 }
             }
         }
+
         return 0;
     }
 }
