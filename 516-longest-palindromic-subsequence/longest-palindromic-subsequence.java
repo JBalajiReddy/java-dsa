3785c4c702f085aa32d070cb765ec7333a04a7f5
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-09-22 14:06:55 +0530

    Time: 59 ms (27.73%) | Memory: 55.4 MB (30.99%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,50 +1,21 @@
-//bottom-up
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        StringBuilder str = new StringBuilder(s);
-        return longestPalindrome(s, str.reverse().toString());
-    }
-
-    public int longestPalindrome(String s, String rS) {
         int n = s.length();
-        int dp[][] = new int[n + 1][n + 1];
+        StringBuilder s1 = new StringBuilder(s);
+        String rs = s1.reverse().toString();
+        int[][] dp = new int[n + 1][n + 1];
         dp[0][0] = 0;
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                if (s.charAt(i - 1) == rS.charAt(j - 1)) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == rs.charAt(j - 1)) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    int c1 = dp[i - 1][j];
+                    int c2 = dp[i][j - 1];
+                    dp[i][j] = Math.max(c1, c2);
                 }
             }
         }
         return dp[n][n];
     }
 }
-
-// top - down
-// class Solution {
-//     public int longestPalindromeSubseq(String s) {
-//         int n = s.length();
-//         int[][] dp = new int[n][n];
-//         return longestPalindrome(s, 0, n - 1, dp);
-//     }
-
-//     public int longestPalindrome(String s, int start, int end, int[][] dp) {
-
-//         if (start > end){
-//             return 0;
-//         }
-//         if(start == end){
-//             return 1;
-//         }
-//         if(dp[start][end] != 0) return dp[start][end];
-
-//         if (s.charAt(start) == s.charAt(end)) {
-//            dp[start][end] = 2 + longestPalindrome(s, start + 1, end - 1, dp);
-//         } else {
-//            dp[start][end] = Math.max(longestPalindrome(s, start + 1, end, dp), longestPalindrome(s, start, end - 1, dp));
-//         }
-//         return dp[start][end];
-//     }
-// }
