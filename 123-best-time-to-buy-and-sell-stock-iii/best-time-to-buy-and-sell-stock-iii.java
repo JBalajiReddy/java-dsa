6fb9a47b0fed41ef3a8fcac0d4838be5b7e062b8
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-09-25 08:19:50 +0530

    Time: 56 ms (47.09%) | Memory: 64.2 MB (33.96%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,28 +1,61 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] memo = new int[n][2][3];
-        for (int a1[][] : memo) {
-            for (int a2[] : a1) {
-                Arrays.fill(a2, -1);
+        int[][][] dp = new int[n + 1][2][3];
+        // for (int buy = 0; buy <= 1; buy++) {
+        //     for (int cap = 0; cap <= 2; cap++) {
+        //         dp[n][buy][cap] = 0; //last day
+        //     }
+        // }
+       
+        // for (int day = 0; day <= n; day++) {
+        //     for (int buy = 0; buy <= 1; buy++) {
+        //         dp[day][buy][0] = 0; //no more transactions left 
+        //     }
+        // }
+     
+        for (int day = n - 1; day >= 0; day--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    if (buy == 1) {
+                        dp[day][buy][cap] = Math.max(-prices[day] + dp[day + 1][0][cap], dp[day + 1][1][cap]); 
+                    } else {
+                        dp[day][buy][cap] = Math.max(prices[day] + dp[day + 1][1][cap - 1], dp[day + 1][0][cap]);
+                    }
+                }
             }
         }
-        return f(0, 1, 2, prices, memo);
-    }
-
-    private int f(int i, int buy, int cap, int[] p, int[][][] memo) {
-        if (i == p.length)
-            return 0;
-        if (cap == 0)
-            return 0;
-        if (memo[i][buy][cap] != -1)
-            return memo[i][buy][cap];
-        if (buy == 1) {
-            return memo[i][buy][cap] = Math.max(-p[i] + f(i + 1, 0, cap, p, memo),
-                    0 + f(i + 1, 1, cap, p, memo));
-        } else {
-            return memo[i][buy][cap] = Math.max(p[i] + f(i + 1, 1, cap - 1, p, memo),
-                    0 + f(i + 1, 0, cap, p, memo));
-        }
+    
+         return dp[0][1][2];
     }
 }
+
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         int[][][] memo = new int[n][2][3];
+//         for (int a1[][] : memo) {
+//             for (int a2[] : a1) {
+//                 Arrays.fill(a2, -1);
+//             }
+//         }
+//         return f(0, 1, 2, prices, memo);
+//     }
+
+//     private int f(int i, int buy, int cap, int[] p, int[][][] memo) {
+//         if (i == p.length)
+//             return 0;
+//         if (cap == 0)
+//             return 0;
+//         if (memo[i][buy][cap] != -1)
+//             return memo[i][buy][cap];
+//         if (buy == 1) {
+//             return memo[i][buy][cap] = Math.max(-p[i] + f(i + 1, 0, cap, p, memo),
+//                     0 + f(i + 1, 1, cap, p, memo));
+//         } else {
+//             return memo[i][buy][cap] = Math.max(p[i] + f(i + 1, 1, cap - 1, p, memo),
+//                     0 + f(i + 1, 0, cap, p, memo));
+//         }
+//     }
+// }
