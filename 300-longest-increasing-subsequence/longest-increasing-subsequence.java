a625e2553edee22a1217f462c1542514625f2710
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-09-26 08:47:55 +0530

    Time: 388 ms (10.64%) | Memory: 76.5 MB (22.96%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,17 +1,38 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp, 1);
-        int maxLen = 1;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], 1 + dp[j]);
-                }
-            }
-            maxLen = Math.max(maxLen, dp[i]);
+        if (n == 0) return 0;
+        int[][] memo = new int[n][n];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
         }
-        return maxLen;
+        return f(0, 0, nums, memo);
+    }
+
+    private int f(int index, int prev_index, int[] nums, int[][] memo) {
+        if (index == nums.length) {
+            return 0;
+        }
+
+        if (memo[index][prev_index] != -1) {
+            return memo[index][prev_index];
+        }
+
+        if (prev_index < index) {
+            int take = 0;
+            if (nums[index] > nums[prev_index]) {
+                take = 1 + f(index + 1, index, nums, memo);
+            }
+            int notTake = f(index + 1, prev_index, nums, memo);
+            return memo[index][prev_index] = Math.max(take, notTake);
+        } 
+        
+        else {
+            // Option A: Start a new subsequence by taking nums[index].
+            int startHere = 1 + f(index + 1, index, nums, memo);
+            // Option B: Skip starting here and try to start from the next index.
+            int startLater = f(index + 1, index + 1, nums, memo);
+            return memo[index][prev_index] = Math.max(startHere, startLater);
+        }
     }
 }
