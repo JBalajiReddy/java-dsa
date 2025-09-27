11f53919e2fa092718da939863bd4affb5d8336a
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-09-27 14:47:49 +0530

    Time: 6 ms (88.65%) | Memory: 44.4 MB (58.53%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,62 +1,80 @@
 class Solution {
-    /**
-     * Calculates the length of the Longest Increasing Subsequence (LIS)
-     * by finding the Longest Common Subsequence (LCS) between the original
-     * array and a sorted, unique version of it.
-     */
     public int lengthOfLIS(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        // Step 1: Create a sorted array of the unique elements from `nums`.
-        // A TreeSet is perfect for this as it stores unique elements in ascending order.
-        Set<Integer> uniqueSortedSet = new TreeSet<>();
+        List<Integer> ls = new ArrayList<>();
         for (int num : nums) {
-            uniqueSortedSet.add(num);
-        }
+            int idx = Collections.binarySearch(ls, num);
+            if (idx < 0) { //ele not found
+                idx = -(idx + 1); //insertion point
+            }
 
-        // Convert the Set to an array to allow for indexed access.
-        int[] sortedUniqueNums = new int[uniqueSortedSet.size()];
-        int i = 0;
-        for (Integer num : uniqueSortedSet) {
-            sortedUniqueNums[i++] = num;
-        }
-
-        // Step 2: Find the length of the LCS between the original array
-        // and the new sorted, unique array.
-        return findLCSLength(nums, sortedUniqueNums);
-    }
-
-    /**
-     * A standard dynamic programming function to find the length of the LCS
-     * between two arrays.
-     */
-    private int findLCSLength(int[] arr1, int[] arr2) {
-        int n = arr1.length;
-        int m = arr2.length;
-
-        // dp[i][j] stores the length of the LCS of arr1[0...i-1] and arr2[0...j-1].
-        int[][] dp = new int[n + 1][m + 1];
-
-        // Build the DP table in a bottom-up manner.
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                // If the current characters match, they are part of the common subsequence.
-                if (arr1[i - 1] == arr2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    // If they don't match, take the maximum from the previous states.
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
+            if (idx == ls.size()) {
+                ls.add(num); //add at last
+            } else {
+                ls.set(idx, num);
             }
         }
-        
-        // The length of the LCS is in the bottom-right cell of the DP table.
-        return dp[n][m];
+        return ls.size();
     }
 }
 
+// class Solution {
+//     /**
+//      * Calculates the length of the Longest Increasing Subsequence (LIS)
+//      * by finding the Longest Common Subsequence (LCS) between the original
+//      * array and a sorted, unique version of it.
+//      */
+//     public int lengthOfLIS(int[] nums) {
+//         if (nums == null || nums.length == 0) {
+//             return 0;
+//         }
+
+//         // Step 1: Create a sorted array of the unique elements from `nums`.
+//         // A TreeSet is perfect for this as it stores unique elements in ascending order.
+//         Set<Integer> uniqueSortedSet = new TreeSet<>();
+//         for (int num : nums) {
+//             uniqueSortedSet.add(num);
+//         }
+
+//         // Convert the Set to an array to allow for indexed access.
+//         int[] sortedUniqueNums = new int[uniqueSortedSet.size()];
+//         int i = 0;
+//         for (Integer num : uniqueSortedSet) {
+//             sortedUniqueNums[i++] = num;
+//         }
+
+//         // Step 2: Find the length of the LCS between the original array
+//         // and the new sorted, unique array.
+//         return findLCSLength(nums, sortedUniqueNums);
+//     }
+
+//     /**
+//      * A standard dynamic programming function to find the length of the LCS
+//      * between two arrays.
+//      */
+//     private int findLCSLength(int[] arr1, int[] arr2) {
+//         int n = arr1.length;
+//         int m = arr2.length;
+
+//         // dp[i][j] stores the length of the LCS of arr1[0...i-1] and arr2[0...j-1].
+//         int[][] dp = new int[n + 1][m + 1];
+
+//         // Build the DP table in a bottom-up manner.
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= m; j++) {
+//                 // If the current characters match, they are part of the common subsequence.
+//                 if (arr1[i - 1] == arr2[j - 1]) {
+//                     dp[i][j] = 1 + dp[i - 1][j - 1];
+//                 } else {
+//                     // If they don't match, take the maximum from the previous states.
+//                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+//                 }
+//             }
+//         }
+
+//         // The length of the LCS is in the bottom-right cell of the DP table.
+//         return dp[n][m];
+//     }
+// }
 
 // class Solution {
 //     public int lengthOfLIS(int[] nums) {
@@ -86,7 +104,7 @@ class Solution {
 //             int notTake = f(index + 1, prev_index, nums, memo);
 //             return memo[index][prev_index] = Math.max(take, notTake);
 //         } 
-        
+
 //         else {
 //             // Option A: Start a new subsequence by taking nums[index].
 //             int startHere = 1 + f(index + 1, index, nums, memo);
