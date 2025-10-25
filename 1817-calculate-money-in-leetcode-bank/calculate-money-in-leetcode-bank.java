2703bdddd92fbfe01002ff386ef3963034ae4887
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-10-25 22:46:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (44.44%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -1,13 +1,16 @@
 class Solution {
     public int totalMoney(int n) {
-        int ans = 0, mon = 1;
-        while (n > 0) {
-            for (int d = 0; d < Math.min(n, 7); d++) {
-                ans += mon + d;
-            }
-            n -= 7;
-            mon++;
+        int k = n / 7;
+        int F = 28;
+        int L = 28 + (k - 1) * 7; //ak = a1 + (k - 1) * d
+        int arithmeticSum = k * (F + L) / 2;
+        
+        int monday = 1 + k;
+        int finalWeek = 0;
+        for (int day = 0; day < n % 7; day++) {
+            finalWeek += monday + day;
         }
-        return ans;
+        
+        return arithmeticSum + finalWeek;
     }
 }
