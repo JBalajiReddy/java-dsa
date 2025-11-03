5879116caae072d86a557f180fc491b88164c7e4
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-11-03 15:46:57 +0530

    Time: 2 ms (84.80%) | Memory: 57.1 MB (5.00%) - LeetSync

--- leetcode-java/1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ leetcode-java/1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -13,39 +13,58 @@
  *     }
  * }
  */
+
 class Solution {
-    class Pair {
-        TreeNode node;
-        int max;
-        Pair(TreeNode node, int max) {
-            this.node = node;
-            this.max = max;
-        }
-    }
-
+    int cnt = 0;
     public int goodNodes(TreeNode root) {
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(root, root.val));
-        int cnt = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                Pair p = q.poll();
-                TreeNode node = p.node;
-                int max = p.max;
-
-                if (node.val >= max) cnt++;
-                max = Math.max(max, node.val);
-
-                if (node.left != null) {
-                    q.offer(new Pair(node.left, max));
-                }
-
-                if (node.right != null) {
-                    q.offer(new Pair(node.right, max));
-                }
-            }
-        }
+        dfs(root, root.val);
         return cnt;
     }
+    
+    public void dfs(TreeNode root, int pathMax) {
+        if (root == null)
+            return;
+        pathMax = Math.max(pathMax, root.val);
+        if (root.val >= pathMax)
+            cnt++;
+        dfs(root.left, pathMax);
+        dfs(root.right, pathMax);
+    }
 }
+
+// class Solution {
+//     class Pair {
+//         TreeNode node;
+//         int max;
+//         Pair(TreeNode node, int max) {
+//             this.node = node;
+//             this.max = max;
+//         }
+//     }
+
+//     public int goodNodes(TreeNode root) {
+//         Queue<Pair> q = new LinkedList<>();
+//         q.offer(new Pair(root, root.val));
+//         int cnt = 0;
+//         while (!q.isEmpty()) {
+//             int size = q.size();
+//             for (int i = 0; i < size; i++) {
+//                 Pair p = q.poll();
+//                 TreeNode node = p.node;
+//                 int max = p.max;
+
+//                 if (node.val >= max) cnt++;
+//                 max = Math.max(max, node.val);
+
+//                 if (node.left != null) {
+//                     q.offer(new Pair(node.left, max));
+//                 }
+
+//                 if (node.right != null) {
+//                     q.offer(new Pair(node.right, max));
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// }
