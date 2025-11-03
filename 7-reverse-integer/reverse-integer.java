db9b6ae54215ea6aa912fd3883a71b0df13de8cb
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-11-03 14:49:45 +0530

    Time: 1 ms (81.90%) | Memory: 42.5 MB (6.49%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,16 +1,42 @@
 class Solution {
     public int reverse(int x) {
-        boolean isNeg = x < 0 ? true : false;
-        int y = Math.abs(x);
+        int rev = 0; // This will store the reversed number
+        
+        while (x != 0) {
+            // Get the last digit
+            int pop = x % 10;
+            x /= 10; // Move to the next digit
 
-        long num = 0;
-        while (y != 0) {
-            int d = y % 10;
-            num = num * 10 + d;
-            y /= 10;
+            // --- Check for overflow BEFORE we multiply ---
+
+            // 1. Positive Overflow Check
+            // If rev > 214748364 (which is MAX_VALUE / 10), then
+            // rev * 10 will definitely overflow.
+            if (rev > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            // If rev == 214748364, it overflows only if the next
+            // digit (pop) is greater than 7 (the last digit of MAX_VALUE).
+            if (rev == Integer.MAX_VALUE / 10 && pop > 7) {
+                return 0;
+            }
+
+            // 2. Negative Overflow Check
+            // If rev < -214748364 (which is MIN_VALUE / 10), then
+            // rev * 10 will definitely overflow.
+            if (rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            // If rev == -214748364, it overflows only if the next
+            // digit (pop) is less than -8 (the last digit of MIN_VALUE).
+            if (rev == Integer.MIN_VALUE / 10 && pop < -8) {
+                return 0;
+            }
+            
+            // --- If no overflow, build the reversed number ---
+            rev = rev * 10 + pop;
         }
-
-        if (num >= Integer.MAX_VALUE || num < Integer.MIN_VALUE) return 0;
-        return isNeg ? (int) -num : (int) num;
+        
+        return rev;
     }
 }
