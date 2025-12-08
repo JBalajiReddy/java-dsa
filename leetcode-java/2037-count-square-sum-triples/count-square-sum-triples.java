5fd7332a75dda770cbdeb87c308719de51a8025a
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-12-08 12:03:40 +0530

    Time: 15 ms (39.46%) | Memory: 41.5 MB (67.85%) - LeetSync

--- leetcode-java/2037-count-square-sum-triples/count-square-sum-triples.java
+++ leetcode-java/2037-count-square-sum-triples/count-square-sum-triples.java
@@ -3,11 +3,9 @@ class Solution {
         int cnt = 0;
         for (int a = 1; a <= n; a++) {
             for (int b = 1; b <= n; b++) {
-                for (int c = 1; c <= n; c++) {
-                    if (a != b && b != c && (a * a + b * b == c * c)) {
-                        cnt++;
-                    }
-                }
+                int c = (int) Math.sqrt(a * a + b * b + 1.0);
+                if (c <= n && c * c == a * a + b * b)
+                    cnt++;
             }
         }
         return cnt;
