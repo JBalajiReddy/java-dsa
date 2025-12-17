27157a331e2ece0868fa5c1eb9e71883af59261e
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-12-17 16:04:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (17.11%) - LeetSync

--- leetcode-java/371-sum-of-two-integers/sum-of-two-integers.java
+++ leetcode-java/371-sum-of-two-integers/sum-of-two-integers.java
@@ -1,11 +1,23 @@
 class Solution {
     public int getSum(int a, int b) {
-        int carry = 0;
         while (b != 0) {
-            carry = a & b;
-            a = a ^ b;
-            b = carry << 1;
+            int tmp = a ^ b;
+            int carry = (a & b) << 1;
+            a = tmp;
+            b = carry;
         }
         return a;
     }
 }
+
+// class Solution {
+//     public int getSum(int a, int b) {
+//         int carry = 0;
+//         while (b != 0) {
+//             carry = a & b;
+//             a = a ^ b;
+//             b = carry << 1;
+//         }
+//         return a;
+//     }
+// }
