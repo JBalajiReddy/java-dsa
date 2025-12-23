6b581e4207476c8f9e438eff888c3244dc5e3d7d
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-12-23 12:07:34 +0530

    Time: 40 ms (90.50%) | Memory: 174.5 MB (54.50%) - LeetSync

---
+++ leetcode-java/2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxTwoEvents(int[][] events) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
+        int maxVal = 0, maxSum = 0;
+        for (int[] e : events) {
+            while (!pq.isEmpty() && pq.peek()[1] < e[0]) {
+                maxVal = Math.max(maxVal, pq.peek()[2]);
+                pq.poll();
+            }
+            maxSum = Math.max(maxSum, maxVal + e[2]);
+            pq.offer(e);
+        }
+        return maxSum;
+    }
+}
