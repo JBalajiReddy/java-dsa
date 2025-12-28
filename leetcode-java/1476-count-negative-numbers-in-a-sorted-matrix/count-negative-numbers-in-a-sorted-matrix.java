edb5989e037b80cd93ab93f143aa715d380c1d5b
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-12-28 12:31:37 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (7.96%) - LeetSync

--- leetcode-java/1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
+++ leetcode-java/1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -2,19 +2,37 @@ class Solution {
     public int countNegatives(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         int cnt = 0;
-        for (int[] row : grid) {
-            int low = 0;
-            int high = n - 1;
-            while (low <= high) {
-                int mid = low + (high - low) / 2; 
-                if (row[mid] >= 0) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1; 
-                }
+        int row = m - 1, col = 0;
+        while (row >= 0 && col < n) {
+            int ele = grid[row][col];
+            if (ele < 0) {
+                cnt += (n - col);
+                row--;
+            } else {
+                col++;
             }
-            cnt += (n - low); 
-        }
+        } 
         return cnt;
     }
 }
+
+// class Solution {
+//     public int countNegatives(int[][] grid) {
+//         int m = grid.length, n = grid[0].length;
+//         int cnt = 0;
+//         for (int[] row : grid) {
+//             int low = 0;
+//             int high = n - 1;
+//             while (low <= high) {
+//                 int mid = low + (high - low) / 2; 
+//                 if (row[mid] >= 0) {
+//                     low = mid + 1;
+//                 } else {
+//                     high = mid - 1; 
+//                 }
+//             }
+//             cnt += (n - low); 
+//         }
+//         return cnt;
+//     }
+// }
