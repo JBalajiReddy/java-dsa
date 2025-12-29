0e7ce3bc4827dde0efa2dbc0abf357310331646a
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2025-12-29 16:14:55 +0530

    Time: 86 ms (75.80%) | Memory: 49.1 MB (28.03%) - LeetSync

--- leetcode-java/757-pyramid-transition-matrix/pyramid-transition-matrix.java
+++ leetcode-java/757-pyramid-transition-matrix/pyramid-transition-matrix.java
@@ -1,7 +1,9 @@
 class Solution {
     Map<String, List<String>> mp;
+    Set<String> failedMemo;
     public boolean pyramidTransition(String bottom, List<String> allowed) {
         mp = new HashMap<>();
+        failedMemo = new HashSet<>();
 
         for (String str : allowed) {
             String lr = str.substring(0, 2);
@@ -17,7 +19,12 @@ class Solution {
         if (currRow.length() == 1)
             return true;
         if (nextRow.length() == currRow.length() - 1) {
-            return solve(nextRow, "", 0);
+            if (failedMemo.contains(nextRow))
+                return false;
+            if (solve(nextRow, "", 0))
+                return true;
+            failedMemo.add(nextRow);
+            return false;
         }
 
         String key = currRow.substring(idx, idx + 2);
@@ -31,3 +38,37 @@ class Solution {
         return false;
     }
 }
+
+
+// class Solution {
+//     public boolean pyramidTransition(String bottom, List<String> allowed) {
+//         Map<String, List<Character>> map = new HashMap<>();
+//         for(String s: allowed){
+//             String key = s.substring(0,2); char val = s.charAt(2);
+//             if(!map.containsKey(key)){
+//                 map.put(key, new ArrayList<>());
+//             } 
+//             map.get(key).add(val);
+//         }
+//         StringBuilder sb = new StringBuilder();
+//         return backtrack(bottom, map, sb, 0);
+//     }
+//     public boolean backtrack(String bottom, Map<String, List<Character>> map, StringBuilder sb, int idx){
+//         if(bottom.length() == 1) return true;
+//         if(sb.length() == bottom.length()-1){
+//             return backtrack(sb.toString(), map, new StringBuilder(), 0);
+//         }
+//         String key = bottom.substring(idx, idx+2);
+//         if(!map.containsKey(key)){
+//             return false;
+//         }
+//         for(char ch: map.get(key)){
+//             sb.append(ch);
+//             if(backtrack(bottom, map, sb, idx+1)){
+//                 return true;
+//             }
+//             sb.deleteCharAt(idx);
+//         }
+//         return false;
+//     }
+// }
