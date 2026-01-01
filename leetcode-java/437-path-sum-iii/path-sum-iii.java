107bb1a57adf5572afa83b098895637db014e947
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-01-01 13:52:38 +0530

    Time: 3 ms (97.31%) | Memory: 46.4 MB (24.66%) - LeetSync

--- leetcode-java/437-path-sum-iii/path-sum-iii.java
+++ leetcode-java/437-path-sum-iii/path-sum-iii.java
@@ -13,23 +13,45 @@
  *     }
  * }
  */
+
 class Solution {
-    private int cnt = 0;
+    private Map<Long, Integer> mp;
     public int pathSum(TreeNode root, int targetSum) {
-        if (root == null)
-            return 0;
-        dfs(root, targetSum);
-        pathSum(root.left, targetSum);
-        pathSum(root.right, targetSum);
-        return cnt;
+        mp = new HashMap<>();
+        mp.put(0L, 1);
+        return backtrack(root, 0L, targetSum);
     }
 
-    private void dfs(TreeNode root, long target) {
-        if (root == null)
-            return;
-        if (target == root.val)
-            cnt++;
-        dfs(root.left, target - root.val);
-        dfs(root.right, target - root.val);
+    private int backtrack(TreeNode node, long currSum, int target) {
+        if (node == null)
+            return 0;
+        currSum += node.val;
+        int cnt = mp.getOrDefault(currSum - target, 0);
+        mp.put(currSum, mp.getOrDefault(currSum, 0) + 1);
+        cnt += backtrack(node.left, currSum, target);
+        cnt += backtrack(node.right, currSum, target);
+        mp.put(currSum, mp.getOrDefault(currSum, 0) - 1);
+        return cnt;
     }
 }
+
+// class Solution {
+//     private int cnt = 0;
+//     public int pathSum(TreeNode root, int targetSum) {
+//         if (root == null)
+//             return 0;
+//         dfs(root, targetSum);
+//         pathSum(root.left, targetSum);
+//         pathSum(root.right, targetSum);
+//         return cnt;
+//     }
+
+//     private void dfs(TreeNode root, long target) {
+//         if (root == null)
+//             return;
+//         if (target == root.val)
+//             cnt++;
+//         dfs(root.left, target - root.val);
+//         dfs(root.right, target - root.val);
+//     }
+// }
