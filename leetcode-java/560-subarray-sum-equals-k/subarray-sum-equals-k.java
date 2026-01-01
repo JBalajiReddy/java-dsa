e5dbe3270d75fef84ef93462f5136a836eace674
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-01-01 14:18:50 +0530

    Time: 26 ms (34.17%) | Memory: 49.1 MB (21.26%) - LeetSync

--- leetcode-java/560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ leetcode-java/560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,23 +1,14 @@
-//The number of times sum - k has occurred so far
-//tells us how many subarrays ending at the current index have a total sum equal to k.
-
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n = nums.length;
-        Map<Integer, Integer> mp = new HashMap<>();
-        mp.put(0, 1); // handles cases where a subarray starts from index 0
-        //sum == k -> sum - k = 0
-
-        int res = 0, sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-
-            if (mp.containsKey(sum - k))
-                res += mp.get(sum - k);
-
-            mp.put(sum, mp.getOrDefault(sum, 0) + 1);
+        Map<Long, Integer> mp = new HashMap<>();
+        int cnt = 0;
+        long currSum = 0;
+        mp.put(0L, 1);
+        for (int n : nums) {
+            currSum += n;
+            cnt += mp.getOrDefault(currSum - k, 0);
+            mp.put(currSum, mp.getOrDefault(currSum, 0) + 1);
         }
-        return res;
+        return cnt;
     }
 }
