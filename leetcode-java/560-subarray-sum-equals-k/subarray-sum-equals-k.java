b0776076abdfd8f75e87ed3d0a36145ec4487bfd
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-01-01 15:20:27 +0530

    Time: 27 ms (29.84%) | Memory: 49.6 MB (6.76%) - LeetSync

--- leetcode-java/560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ leetcode-java/560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -6,7 +6,14 @@ class Solution {
         mp.put(0L, 1);
         for (int n : nums) {
             currSum += n;
+            // We want a subarray ending here with sum 'k'.
+            // Equation: currSum - oldSum = k  =>  oldSum = currSum - k
+            // We check if we have seen 'currSum - k' before.
+
+            //the value in the map tells us HOW MANY times 
+            // that specific oldSum occurred. We add that count to our total.
             cnt += mp.getOrDefault(currSum - k, 0);
+            
             mp.put(currSum, mp.getOrDefault(currSum, 0) + 1);
         }
         return cnt;
