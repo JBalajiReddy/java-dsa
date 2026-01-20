a40081feb1b17c06fe7ed9c8fb8c977e41340920
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-01-20 13:58:25 +0530

    Time: 1 ms (100.00%) | Memory: 46.9 MB (30.70%) - LeetSync

--- leetcode-java/3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
+++ leetcode-java/3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
@@ -1,17 +1,18 @@
 class Solution {
+
     public int[] minBitwiseArray(List<Integer> nums) {
         int n = nums.size();
-        int[] res = new int[n];
+        int[] result = new int[n];
         for (int i = 0; i < n; i++) {
-            res[i] = getNum(nums.get(i));
+            int x = nums.get(i);
+            int res = -1;
+            int d = 1;
+            while ((x & d) != 0) {
+                res = x - d;
+                d <<= 1;
+            }
+            result[i] = res;
         }
-        return res;
-    }
-
-    private int getNum(int target) {
-        for (int i = 0; i < target; i++) {
-            if ((i | i + 1) == target) return i;
-        }
-        return -1;
+        return result;
     }
 }
