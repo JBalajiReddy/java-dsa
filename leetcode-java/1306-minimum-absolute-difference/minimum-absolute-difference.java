274068447d6978a0a494b59ad564a8286864d901
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-01-26 12:10:36 +0530

    Time: 20 ms (97.68%) | Memory: 63.9 MB (61.46%) - LeetSync

--- leetcode-java/1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ leetcode-java/1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -1,21 +1,19 @@
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(arr);
-        int n = arr.length, minD = 1000_000_000; 
-        Set<Integer> set = new HashSet<>();
-        for (int i = 1; i < n; i++) {
-            minD = Math.min(minD, (arr[i] - arr[i - 1]));
-            set.add(arr[i]);
-        }
-        List<List<Integer>> ls = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (set.contains(minD + arr[i])) {
-                List<Integer> tmp = new ArrayList<>();
-                tmp.add(arr[i]);
-                tmp.add(arr[i] + minD);
-                ls.add(tmp);
+        int min = 1000_000_000;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+            if (diff < min) {
+                min = diff;
+                list.clear();
+            }
+            if (diff == min) {
+                list.add(Arrays.asList(arr[i], arr[i + 1]));
             }
         }
-        return ls;
+        return list;
     }
 }
