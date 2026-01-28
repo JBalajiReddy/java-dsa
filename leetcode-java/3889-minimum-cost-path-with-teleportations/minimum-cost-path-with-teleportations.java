99933764fd514e69ee2ac9e05009247eb0177133
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-01-28 14:11:19 +0530

    Time: 186 ms (40.58%) | Memory: 48.1 MB (60.87%) - LeetSync

--- leetcode-java/3889-minimum-cost-path-with-teleportations/minimum-cost-path-with-teleportations.java
+++ leetcode-java/3889-minimum-cost-path-with-teleportations/minimum-cost-path-with-teleportations.java
@@ -4,15 +4,15 @@ class Solution {
     public int minCost(int[][] grid, int K) {
         int n = grid.length, m = grid[0].length;
         // Assumption: Grid values are between 0 and 10000.
-        int range = 10000; 
+        int range = 10000;
 
         // dp[p][i][j] stores the minimum cost to reach the bottom-right from (i, j)
         // using exactly p "teleport" charges.
-        int[][][] dp = new int[K+1][n][m];
-        
+        int[][][] dp = new int[K + 1][n][m];
+
         // Initialize DP table with MAX (infinity)
-        for(int p=0; p<=K; p++){
-            for(int i=0; i<n; i++){
+        for (int p = 0; p <= K; p++) {
+            for (int i = 0; i < n; i++) {
                 Arrays.fill(dp[p][i], MAX);
             }
         }
@@ -20,48 +20,48 @@ class Solution {
         // bestPrefix[v] will store the minimum cost found in the PREVIOUS layer (p-1)
         // starting from any cell with value <= v.
         // This optimizes the jump calculation from O(N*M) to O(1).
-        int[] bestPrefix = new int[range+1];
+        int[] bestPrefix = new int[range + 1];
         Arrays.fill(bestPrefix, MAX);
 
         // Iterate through each layer of allowed jumps (0 to K)
-        for(int p=0; p<=K; p++){
-            
+        for (int p = 0; p <= K; p++) {
+
             // bestCurrent[v] tracks the min cost for the CURRENT layer p 
             // for cells with exact value v. Used to build bestPrefix for the next layer.
-            int[] bestCurrent = new int[range+1];
+            int[] bestCurrent = new int[range + 1];
             Arrays.fill(bestCurrent, MAX);
 
             // Traverse grid bottom-up (from target to start)
-            for(int i=n-1; i>=0; i--){
-                for(int j=m-1; j>=0; j--){
-                    
+            for (int i = n - 1; i >= 0; i--) {
+                for (int j = m - 1; j >= 0; j--) {
+
                     // Base Case: We are at the target. Cost is 0.
-                    if(i==n-1 && j==m-1){
+                    if (i == n - 1 && j == m - 1) {
                         dp[p][i][j] = 0;
                     } else {
                         int ans = MAX;
 
                         // --- OPTION 1: Physical Walk (Down or Right) ---
                         // Move Down: Cost is value of cell (i+1, j) + remaining path cost
-                        if(i+1<n){
-                            ans = Math.min(ans, grid[i+1][j] + dp[p][i+1][j]);
+                        if (i + 1 < n) {
+                            ans = Math.min(ans, grid[i + 1][j] + dp[p][i + 1][j]);
                         }
                         // Move Right: Cost is value of cell (i, j+1) + remaining path cost
-                        if(j+1<m){
-                            ans = Math.min(ans, grid[i][j+1] + dp[p][i][j+1]);
+                        if (j + 1 < m) {
+                            ans = Math.min(ans, grid[i][j + 1] + dp[p][i][j + 1]);
                         }
 
                         // --- OPTION 2: Teleport/Jump (consumes 1 charge) ---
                         // If we have used at least 1 charge (p > 0), we could have arrived here via a jump.
                         // We check the best cost from the previous layer (p-1) for any destination 
                         // cell (x,y) where grid[x][y] <= grid[i][j].
-                        if(p>0){
+                        if (p > 0) {
                             ans = Math.min(ans, bestPrefix[grid[i][j]]);
                         }
 
                         dp[p][i][j] = ans;
                     }
-                    
+
                     // Update bestCurrent for the specific value of the current cell.
                     // This records that we found a path with cost dp[p][i][j] starting at a cell with value grid[i][j].
                     bestCurrent[grid[i][j]] = Math.min(bestCurrent[grid[i][j]], dp[p][i][j]);
@@ -73,11 +73,11 @@ class Solution {
             // covers all values from 0 to v.
             Arrays.fill(bestPrefix, MAX);
             bestPrefix[0] = bestCurrent[0];
-            for(int r=1; r<=range; r++){
-                bestPrefix[r] = Math.min(bestPrefix[r-1], bestCurrent[r]);
+            for (int r = 1; r <= range; r++) {
+                bestPrefix[r] = Math.min(bestPrefix[r - 1], bestCurrent[r]);
             }
         }
-        
+
         // Return min cost starting from (0,0) with K jumps allowed.
         return dp[K][0][0];
     }
