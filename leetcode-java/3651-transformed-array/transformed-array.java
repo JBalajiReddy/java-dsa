68f9b520ed6e5cb381adb1c232b6450fea61d916
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-02-05 09:12:59 +0530

    Time: 1 ms (98.72%) | Memory: 47 MB (13.78%) - LeetSync

--- leetcode-java/3651-transformed-array/transformed-array.java
+++ leetcode-java/3651-transformed-array/transformed-array.java
@@ -3,16 +3,27 @@ class Solution {
         int n = nums.length;
         int[] res = new int[n];
         for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            int stepIdx = i;
-            if (num > 0) {
-                res[i] = nums[(stepIdx + nums[i]) % n];
-            } else if (num < 0) {
-                res[i] = nums[((stepIdx + nums[i]) % n + n) % n];
-            } else {
-                res[i] = nums[i];
-            }
+            res[i] = nums[((i + nums[i]) % n + n) % n];
         }
         return res;
     }
 }
+
+// class Solution {
+//     public int[] constructTransformedArray(int[] nums) {
+//         int n = nums.length;
+//         int[] res = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             int num = nums[i];
+//             int stepIdx = i;
+//             if (num > 0) {
+//                 res[i] = nums[(stepIdx + nums[i]) % n];
+//             } else if (num < 0) {
+//                 res[i] = nums[((stepIdx + nums[i]) % n + n) % n];
+//             } else {
+//                 res[i] = nums[i];
+//             }
+//         }
+//         return res;
+//     }
+// }
