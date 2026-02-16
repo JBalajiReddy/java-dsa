4f6ba415fc900c419e9e93b910d5ed74f8b97a36
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-02-16 15:12:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (19.19%) - LeetSync

--- leetcode-java/190-reverse-bits/reverse-bits.java
+++ leetcode-java/190-reverse-bits/reverse-bits.java
@@ -4,9 +4,21 @@ class Solution {
         int res = 0;
         for (int i = 0; i < 32; i++) {
             res <<= 1;
-            res = res | (n & 1);
+            res |= (n & 1);
             n >>= 1; 
         }
         return res;
     }
 }
+
+
+// class Solution {
+//     public int reverseBits(int n) {
+//         int ans = 0, mask = 1;
+//         for (int i = 0; i < 32; i++) {
+//             if ((n & mask) != 0) ans += 1 << 31 - i;
+//             mask = mask << 1;
+//         }
+//         return ans;
+//     }
+// }
