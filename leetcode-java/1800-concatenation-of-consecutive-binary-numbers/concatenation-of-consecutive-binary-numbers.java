8810910577b3cd6b015c8f1dcf69cb9429a1f0b1
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-02-28 15:04:41 +0530

    Time: 41 ms (50.00%) | Memory: 42.5 MB (73.26%) - LeetSync

--- leetcode-java/1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
+++ leetcode-java/1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -1,14 +1,14 @@
 class Solution {
     public int concatenatedBinary(int n) {
         int mod = 1000_000_007;
-        StringBuilder sb = new StringBuilder();
+        int digitsToShift = 0;
+        long res = 0;
         for (int i = 1; i <= n; i++) {
-            sb.append(Integer.toBinaryString(i));
+            //int digitsToShift = (int)(Math.log(i) / Math.log(2)) + 1;
+            if ((i & (i - 1)) == 0)
+                digitsToShift++; //power of 2 check
+            res = ((res << digitsToShift) % mod + i) % mod;
         }
-        long num = 0;
-        for (int i = 0; i < sb.length(); i++) {
-            num = (num * 2 + sb.charAt(i) - '0') % mod;
-        }
-        return (int) num;
+        return (int)res;
     }
 }
