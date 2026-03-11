f6d78586598d1a85458603a3ef9755d6b94c9d9c
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-03-11 20:37:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (50.09%) - LeetSync

---
+++ leetcode-java/1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        if (n == 0)
+            return 1;
+        int original = n;
+        int num = 0;
+        int i = 0;
+        while (n > 0) {
+            int digit = (n & 1);
+            n = n >> 1;
+            int comp = (digit == 0) ? 1 : 0;
+            num = num + ((int) Math.pow(2, i) * comp);
+            i++;
+        }
+        return num;
+    }
+}
