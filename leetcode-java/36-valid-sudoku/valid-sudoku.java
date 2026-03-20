f96bd5c8903796f21e9999b54d4e9f50a1867577
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-03-20 15:19:22 +0530

    Time: 12 ms (18.37%) | Memory: 52.3 MB (18.74%) - LeetSync

--- leetcode-java/36-valid-sudoku/valid-sudoku.java
+++ leetcode-java/36-valid-sudoku/valid-sudoku.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> seen = new HashSet<>();
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                char num = board[i][j];
-                if (num != '.') {
-                    if (!seen.add(num + "in row" + i) ||
-                            !seen.add(num + "in column" + j) ||
-                            !seen.add(num + "in block" + i / 3 + "-" + j / 3))
+                char ch = board[i][j];
+                if (ch != '.') {
+                    //already in hashset
+                    if (!set.add(ch + " in same row " + i) ||
+                            !set.add(ch + " in same col " + j) ||
+                                    !set.add(ch + " in same block " + i / 3 + j / 3)) {
                         return false;
+                    }
                 }
             }
         }
