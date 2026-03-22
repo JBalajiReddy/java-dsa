0aec15ca1d39b8d8953cd0374162cf81bdf1167b
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-03-22 12:13:58 +0530

    Time: 2 ms (99.24%) | Memory: 44.2 MB (90.11%) - LeetSync

--- leetcode-java/125-valid-palindrome/valid-palindrome.java
+++ leetcode-java/125-valid-palindrome/valid-palindrome.java
@@ -1,34 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty())
-            return true;
-
-        int n = s.length();
-        int i = 0;
-        int j = n - 1;
-
-        while (i <= j) {
-            char a = s.charAt(i);
-            char b = s.charAt(j);
-            if (!Character.isLetterOrDigit(a))
-                i++;
-            else if (!Character.isLetterOrDigit(b))
-                j--;
-            else {
-                if (Character.toLowerCase(a) != Character.toLowerCase(b))
-                    return false;
-                i++;
-                j--;
+        int l = 0, r = s.length() - 1;
+        
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
             }
+            
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+            }
+            
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false; 
+            }
+            
+            l++;
+            r--;
         }
+        
         return true;
     }
 }
-
-// public class Solution {
-//     public boolean isPalindrome(String s) {
-//         String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-//         String rev = new StringBuffer(actual).reverse().toString();
-//         return actual.equals(rev);
-//     }
-// }
