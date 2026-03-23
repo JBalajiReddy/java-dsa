9d8d7714bf29d782468019bf3d59bc06af55750b
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-03-23 22:00:54 +0530

    Time: 1 ms (95.83%) | Memory: 43.2 MB (44.38%) - LeetSync

--- leetcode-java/1894-merge-strings-alternately/merge-strings-alternately.java
+++ leetcode-java/1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,24 +1,13 @@
-class Solution {
+public class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int s1 = word1.length();
-        int s2 = word2.length();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < Math.min(s1, s2); i++) {
-            char a = word1.charAt(i);
-            char b = word2.charAt(i);
-            sb.append(a);
-            sb.append(b);
+        StringBuilder res = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < word1.length() && j < word2.length()) {
+            res.append(word1.charAt(i++));
+            res.append(word2.charAt(j++));
         }
-
-        if (s1 > s2) {
-            for (int i = s2; i < s1; i++) {
-                sb.append(word1.charAt(i));
-            }
-        } else if (s2 > s1) {
-            for (int i = s1; i < s2; i++) {
-                sb.append(word2.charAt(i));
-            }
-        }
-        return sb.toString();
+        res.append(word1.substring(i));
+        res.append(word2.substring(j));
+        return res.toString();
     }
 }
