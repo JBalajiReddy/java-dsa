61c53c61c8c27712ad20f0eaa8efdd9d2783e806
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-03-23 22:01:52 +0530

    Time: 2 ms (24.21%) | Memory: 43 MB (73.99%) - LeetSync

--- leetcode-java/1894-merge-strings-alternately/merge-strings-alternately.java
+++ leetcode-java/1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,13 +1,29 @@
 public class Solution {
     public String mergeAlternately(String word1, String word2) {
+        int n = word1.length(), m = word2.length();
         StringBuilder res = new StringBuilder();
-        int i = 0, j = 0;
-        while (i < word1.length() && j < word2.length()) {
-            res.append(word1.charAt(i++));
-            res.append(word2.charAt(j++));
+        for (int i = 0; i < n || i < m; i++) {
+            if (i < n) {
+                res.append(word1.charAt(i));
+            }
+            if (i < m) {
+                res.append(word2.charAt(i));
+            }
         }
-        res.append(word1.substring(i));
-        res.append(word2.substring(j));
         return res.toString();
     }
 }
+
+// public class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+//         StringBuilder res = new StringBuilder();
+//         int i = 0, j = 0;
+//         while (i < word1.length() && j < word2.length()) {
+//             res.append(word1.charAt(i++));
+//             res.append(word2.charAt(j++));
+//         }
+//         res.append(word1.substring(i));
+//         res.append(word2.substring(j));
+//         return res.toString();
+//     }
+// }
