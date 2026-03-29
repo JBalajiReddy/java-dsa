37e4d7483f88c495f709c48ca74e1272ef505076
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-03-29 14:18:46 +0530

    Time: 6 ms (88.07%) | Memory: 43.9 MB (81.96%) - LeetSync

--- leetcode-java/567-permutation-in-string/permutation-in-string.java
+++ leetcode-java/567-permutation-in-string/permutation-in-string.java
@@ -2,68 +2,36 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        if (m < n)
-            return false;
-        int map1[] = new int[26];
-        int map2[] = new int[26];
+        if (m < n) return false;
+        int[] mp1 = new int[26];
+        int[] mp2 = new int[26];
         for (int i = 0; i < n; i++) {
-            map1[s1.charAt(i) - 'a']++;
-            map2[s2.charAt(i) - 'a']++;
+            mp1[s1.charAt(i) - 'a']++;
+            mp2[s2.charAt(i) - 'a']++;
         }
-        //initial window   
-        if (isMatched(map1, map2)) {
+
+        //1st window
+        if (isMatched(mp1, mp2)) {
             return true;
         }
 
-        //next windows
+        //later windows
         for (int i = 1; i <= m - n; i++) {
-            map2[s2.charAt(i - 1) - 'a']--; // del
-            map2[s2.charAt(i + n - 1) - 'a']++;// add
-            if (isMatched(map1, map2)) {
+            mp2[s2.charAt(i - 1) - 'a']--; //remove left char
+            mp2[s2.charAt(i + n - 1) - 'a']++; //add righter char
+            if (isMatched(mp1, mp2)) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isMatched(int map1[], int map2[]) {
+    private boolean isMatched(int[] m1, int[] m2) {
         for (int i = 0; i < 26; i++) {
-            if (map1[i] != map2[i])
+            if (m1[i] != m2[i]) {
                 return false;
+            }
         }
         return true;
     }
 }
-
-// class Solution {
-//     public boolean checkInclusion(String s1, String s2) {
-//         int[] freq = new int[26];
-//         for (int i = 0; i < s1.length(); i++)
-//             freq[s1.charAt(i) - 'a']++;
-
-//         int winSize = s1.length();
-//         for (int i = 0; i < s2.length(); i++) {
-//             int[] winFreq = new int[26];
-//             int winIdx = 0, idx = i;
-
-//             while (winIdx < winSize && idx < s2.length()) {
-//                 winFreq[s2.charAt(idx) - 'a']++;
-//                 winIdx++;
-//                 idx++;
-//             }
-
-//             if (isSameFreq(freq, winFreq))
-//                 return true;
-//         }
-//         return false;
-//     }
-
-//     private boolean isSameFreq(int[] f1, int[] f2) {
-//         for (int i = 0; i < 26; i++) {
-//             if (f1[i] != f2[i])
-//                 return false;
-//         }
-
-//         return true;
-//     }
-// }
