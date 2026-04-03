cf731db93d9baec2a3c4199a85d16619885176f6
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-04-03 21:27:21 +0530

    Time: 31 ms (53.36%) | Memory: 155.8 MB (24.18%) - LeetSync

--- leetcode-java/239-sliding-window-maximum/sliding-window-maximum.java
+++ leetcode-java/239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,31 +1,27 @@
-//NGE - monotonic decreasing
-class Solution {
+public class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        if (n == 1 || k == 1)
-            return nums;
-        Deque<Integer> dq = new ArrayDeque<>(); //store index
-        int ri = 0;
-        int[] res = new int[n - k + 1];
+        int[] output = new int[n - k + 1];
+        Deque<Integer> q = new LinkedList<>();
+        int l = 0, r = 0;
 
-        for (int i = 0; i < n; i++) {
+        while (r < n) {
+            while (!q.isEmpty() && nums[q.getLast()] < nums[r]) {
+                q.removeLast();
+            }
+            q.addLast(r);
 
-            //remove out-of-bound indices
-            if (!dq.isEmpty() && dq.getFirst() <= i - k)
-                dq.removeFirst();
+            if (l > q.getFirst()) {
+                q.removeFirst();
+            }
 
-            // remove smaller numbers in k range as they are useless
-            while (!dq.isEmpty() && nums[dq.getLast()] <= nums[i])
-                dq.removeLast();
-
-            dq.addLast(i);
-
-            if (i >= k - 1)
-                res[ri++] = nums[dq.getFirst()]; //max in each window
+            if ((r + 1) >= k) {
+                output[l] = nums[q.getFirst()];
+                l++;
+            }
+            r++;
         }
 
-        return res;
+        return output;
     }
 }
-
-//first ------ last
