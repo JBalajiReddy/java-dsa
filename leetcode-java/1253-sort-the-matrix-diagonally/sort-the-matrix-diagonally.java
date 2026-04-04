e997875a3dd351e833b1d13abd668c9e831cf119
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-04-04 11:42:58 +0530

    Time: 11 ms (47.31%) | Memory: 46.8 MB (48.04%) - LeetSync

---
+++ leetcode-java/1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] diagonalSort(int[][] mat) {
+        int r = mat.length;
+        int c = mat[0].length;
+        int[][] res = new int[r][c];
+        Map<Integer, PriorityQueue<Integer>> mp = new HashMap<>();
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                mp.computeIfAbsent(j - i, k -> new PriorityQueue<>()).add(mat[i][j]);
+            }
+        }
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                int ele = mp.get(j - i).poll();
+                res[i][j] = ele;
+            }
+        }
+
+        res[r - 1][0] = mat[r - 1][0];
+        res[0][c - 1] = mat[0][c - 1];
+        return res;
+    }
+}
