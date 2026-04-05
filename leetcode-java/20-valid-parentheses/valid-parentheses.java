d28d2d90843d4f14b9cc765dc8af98351ec7d868
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-04-05 14:24:59 +0530

    Time: 4 ms (41.15%) | Memory: 43.6 MB (12.46%) - LeetSync

--- leetcode-java/20-valid-parentheses/valid-parentheses.java
+++ leetcode-java/20-valid-parentheses/valid-parentheses.java
@@ -1,22 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
-
-        if (s.length() % 2 != 0)
+        if ((s.length() & 1) != 0)
             return false;
+
         Stack<Character> st = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if (ch == '(' || ch == '[' || ch == '{')
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
                 st.push(ch);
-            else {
-                if (st.isEmpty())
+            } else {
+                if (st.isEmpty()) {
                     return false;
-
-                if ((ch == ')' && st.peek() == '(') ||
-                        (ch == ']' && st.peek() == '[') ||
-                        (ch == '}' && st.peek() == '{')) {
+                } else if ((st.peek() == '(' && ch == ')') || (st.peek() == '[' && ch == ']')
+                        || (st.peek() == '{' && ch == '}')) {
                     st.pop();
                 } else {
                     return false;
