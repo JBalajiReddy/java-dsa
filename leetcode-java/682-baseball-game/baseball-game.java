bf87a47c5567add394a09565e1668f028c7fa5de
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-04-05 14:14:17 +0530

    Time: 3 ms (82.07%) | Memory: 43.6 MB (55.04%) - LeetSync

--- leetcode-java/682-baseball-game/baseball-game.java
+++ leetcode-java/682-baseball-game/baseball-game.java
@@ -1,35 +1,24 @@
 class Solution {
     public int calPoints(String[] operations) {
-        int total = 0;
-        Stack<Integer> st = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         for (String op : operations) {
-            if (op.matches("^[+-]?\\d+$")) {
-                int num = Integer.parseInt(op);
-                st.push(num);
-            } else if (op.equals("+")) {
-                if (st.size() >= 2) {
-                    int num1 = st.pop();
-                    int num2 = st.pop();
-                    int sum = num1 + num2;
-                    st.push(num2);
-                    st.push(num1);
-                    st.push(sum);
-                }
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newTop = top + stack.peek();
+                stack.push(top);
+                stack.push(newTop);
             } else if (op.equals("D")) {
-                if (st.size() >= 1) {
-                    int num1 = st.peek();
-                    st.push(2 * num1);
-                }
+                stack.push(2 * stack.peek());
             } else if (op.equals("C")) {
-                if (st.size() >= 1) {
-                    st.pop();
-                }
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(op));
             }
         }
-
-        for (int s : st) {
-            total += s;
+        int sum = 0;
+        for (int score : stack) {
+            sum += score;
         }
-        return total;
+        return sum;
     }
 }
