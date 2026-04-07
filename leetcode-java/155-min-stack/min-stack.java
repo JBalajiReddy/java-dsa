358eae8d9fd2c88d51a035aeb13086b5899e9d57
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-04-07 21:40:37 +0530

    Time: 5 ms (85.43%) | Memory: 47 MB (74.79%) - LeetSync

--- leetcode-java/155-min-stack/min-stack.java
+++ leetcode-java/155-min-stack/min-stack.java
@@ -1,10 +1,11 @@
 class MinStack {
     Stack<Long> st;
     long min;
+
     public MinStack() {
         st = new Stack<>();
     }
-    
+
     public void push(int val) {
         if (st.isEmpty()) {
             st.push(0L);
@@ -16,7 +17,7 @@ class MinStack {
             }
         }
     }
-    
+
     public void pop() {
         if (st.isEmpty()) {
             return;
@@ -25,8 +26,13 @@ class MinStack {
         if (diff < 0) {
             min = min - diff;
         }
+        // diff = val - oldMin        (what was stored)
+        // val = min                 (since val was the minimum at that point)
+
+        // diff = min - oldMin
+        // oldMin = min - diff        ← this is exactly: min = min - diff
     }
-    
+
     public int top() {
         long diff = st.peek();
         if (diff > 0) {
@@ -35,7 +41,7 @@ class MinStack {
             return (int) min;
         }
     }
-    
+
     public int getMin() {
         return (int) min;
     }
