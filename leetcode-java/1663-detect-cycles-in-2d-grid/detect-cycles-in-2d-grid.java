3b90a9ad76aaf2ff8ae39baec43d14e34674f8a4
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-04-26 11:24:55 +0530

    Time: 19 ms (63.88%) | Memory: 140.1 MB (51.25%) - LeetSync

--- leetcode-java/1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
+++ leetcode-java/1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
@@ -1,16 +1,15 @@
-public class Solution {
-    private static final int[][] DIRS = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+class Solution {
+    private int n, m;
+    private int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private boolean[][] vis;
     public boolean containsCycle(char[][] grid) {
-        if (grid == null || grid.length == 0) return false;
-        
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visit = new boolean[m][n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visit[i][j]) {
-                    if (dfs(grid, visit, i, j, -1, -1, grid[i][j])) {
+        n = grid.length;
+        m = grid[0].length;
+        vis = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j]) {
+                    if (dfs(grid, i, j, -1, -1, grid[i][j])) {
                         return true;
                     }
                 }
@@ -18,37 +17,26 @@ public class Solution {
         }
         return false;
     }
-
-    private boolean dfs(char[][] grid, boolean[][] visit, int r, int c, int pr, int pc, char val) {
-        visit[r][c] = true;
-
-        for (int[] dir : DIRS) {
-            int nr = r + dir[0];
-            int nc = c + dir[1];
-
-            // Explicit boundary checks
-            if (nr < 0 || nr >= grid.length || nc < 0 || nc >= grid[0].length) {
+    private boolean dfs(char[][] grid, int r, int c, int pr, int pc, char val) {
+        vis[r][c] = true;
+        for (int[] d : dirs) {
+            int nr = d[0] + r;
+            int nc = d[1] + c;
+            if (nr < 0 || nr >= n || nc < 0 || nc >= m) {
                 continue;
             }
-
-            // Ignore the parent cell we just came from
             if (nr == pr && nc == pc) {
                 continue;
             }
-
-            // If the neighbor has the same value
             if (grid[nr][nc] == val) {
-                // If it's already visited, a cycle is found
-                if (visit[nr][nc]) {
+                if (vis[nr][nc]) {
                     return true;
                 }
-                // Continue DFS on the unvisited neighbor
-                if (dfs(grid, visit, nr, nc, r, c, val)) {
+                if (dfs(grid, nr, nc, r, c, val)) {
                     return true;
                 }
             }
         }
-
         return false;
     }
 }
