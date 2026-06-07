69e361b8c5160f79bc6fbec8e544bf9a7dd95161
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-06-07 11:05:33 +0530

    Time: 75 ms (21.56%) | Memory: 87.2 MB (14.80%) - LeetSync

--- leetcode-java/2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
+++ leetcode-java/2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -15,35 +15,38 @@
  */
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) {
-        Map<Integer, TreeNode> mp = new HashMap<>();
+        // Maps node value to the actual TreeNode object in memory
+        Map<Integer, TreeNode> nodeMap = new HashMap<>();
+        // Tracks all nodes that have an incoming edge (are children)
         Set<Integer> children = new HashSet<>();
+        
         for (int[] d : descriptions) {
             int parentVal = d[0];
             int childVal = d[1];
             boolean isLeft = d[2] == 1;
-            if (!mp.containsKey(parentVal)) {
-                mp.put(parentVal, new TreeNode(parentVal));
-            }
-
-            if (!mp.containsKey(childVal)) {
-                mp.put(childVal, new TreeNode(childVal));
-            }
-
+            
+            // Create parent and child if they haven't been encountered yet
+            nodeMap.putIfAbsent(parentVal, new TreeNode(parentVal));
+            nodeMap.putIfAbsent(childVal, new TreeNode(childVal));
+            
+            // Connect the child to the parent
             if (isLeft) {
-                mp.get(parentVal).left = mp.get(childVal);
+                nodeMap.get(parentVal).left = nodeMap.get(childVal);
             } else {
-                mp.get(parentVal).right = mp.get(childVal);
+                nodeMap.get(parentVal).right = nodeMap.get(childVal);
             }
-
+            
+            // Mark this value as a child (it cannot be the root)
             children.add(childVal);
         }
-
-        for (TreeNode node : mp.values()) {
+        
+        // The root is the only node in the map that is NOT in the children set
+        for (TreeNode node : nodeMap.values()) {
             if (!children.contains(node.val)) {
                 return node;
             }
         }
-
-        return null;
+        
+        return null; // Fallback, should not be reached with valid input
     }
 }
