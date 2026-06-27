9db3c601a948400f30eb35d2f3f5dbc1b4dd63c6
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-06-27 18:34:31 +0530

    Time: 0 ms (100.00%) | Memory: 80.4 MB (21.20%) - LeetSync

--- leetcode-java/882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ leetcode-java/882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,11 +1,19 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int st = 0, ed = arr.length - 1;
-        while (st < ed) {
-            int mid = st + (ed - st) / 2;
-            if (arr[mid] > arr[mid + 1]) ed = mid; //mid maybe the answer but we look at the left
-            else st = mid + 1;
+        int peakIdx = 0;
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] > arr[m + 1]) {
+                //we are in the decreasing part of the array
+                // so the mid maybe the answer but we look at the left
+                peakIdx = m;
+                r = m - 1;
+            } else {
+                //ascending part
+                l = m + 1;
+            }
         }
-        return st;
+        return peakIdx;
     }
 }
