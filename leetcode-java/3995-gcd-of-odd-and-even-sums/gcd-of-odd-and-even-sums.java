6816d49a1827afa1ffaaa22afb52fe385d1ccbf3
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-07-15 10:49:47 +0530

    Time: 1 ms (83.11%) | Memory: 42.8 MB (6.12%) - LeetSync

---
+++ leetcode-java/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -0,0 +1,15 @@
+// class Solution {
+//     public int gcdOfOddEvenSums(int n) {
+//         return gcd((n * n), n * (n + 1));
+//     }
+
+//     private int gcd(int x, int y) {
+//         return y == 0 ? x : gcd(y, x % y);
+//     }
+// }
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
