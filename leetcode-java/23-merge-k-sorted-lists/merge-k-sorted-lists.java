22956b02a82d9225db18b8db704d3c8e430afd64
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-07-19 17:48:11 +0530

    Time: 1 ms (99.93%) | Memory: 47 MB (28.93%) - LeetSync

--- leetcode-java/23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ leetcode-java/23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,25 +10,69 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
-        for (ListNode ls : lists) {
-            if (ls != null) {
-                pq.offer(ls);
-            }
+        if (lists.length == 0 || lists == null) {
+            return null;
         }
 
+        return divideAndConquer(lists, 0, lists.length - 1);
+    }
+
+    private ListNode divideAndConquer(ListNode[] ls, int start, int end) {
+        if (start == end) {
+            return ls[start];
+        }
+
+        int mid = start + (end - start) / 2;
+
+        ListNode left = divideAndConquer(ls, start, mid);
+        ListNode right = divideAndConquer(ls, mid + 1, end);
+
+        return mergeLists(left, right);
+    }
+
+    private ListNode mergeLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
 
-        while (!pq.isEmpty()) {
-            ListNode node = pq.poll(); //smallest node
-            curr.next = node;
-            curr = curr.next;
-
-            if (node.next != null) {
-                pq.offer(node.next);
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
             }
+
+            curr = curr.next;
         }
+
+        curr.next = (l1 != null) ? l1 : l2;
         return dummy.next;
+
     }
 }
+
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
+//         for (ListNode ls : lists) {
+//             if (ls != null) {
+//                 pq.offer(ls);
+//             }
+//         }
+
+//         ListNode dummy = new ListNode(0);
+//         ListNode curr = dummy;
+
+//         while (!pq.isEmpty()) {
+//             ListNode node = pq.poll(); //smallest node
+//             curr.next = node;
+//             curr = curr.next;
+
+//             if (node.next != null) {
+//                 pq.offer(node.next);
+//             }
+//         }
+//         return dummy.next;
+//     }
+// }
