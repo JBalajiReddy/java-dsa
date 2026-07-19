e5a2c05b34d4dadb421e2be665406ff78f83d3d3
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-07-19 17:16:04 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (69.40%) - LeetSync

--- leetcode-java/316-remove-duplicate-letters/remove-duplicate-letters.java
+++ leetcode-java/316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -1,30 +1,31 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
         int[] freq = new int[26];
-        boolean[] vis = new boolean[26];
-        StringBuilder sb = new StringBuilder();
-
+        boolean[] marked = new boolean[26];
         for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i) - 'a']++;
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (!vis[ch - 'a']) {
+
+            if (!marked[ch - 'a']) {
                 while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
                     if (freq[sb.charAt(sb.length() - 1) - 'a'] > 0) {
-                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        marked[sb.charAt(sb.length() - 1) - 'a'] = false;
                         sb.deleteCharAt(sb.length() - 1);
                     } else {
                         break;
                     }
                 }
-                vis[ch - 'a'] = true;
+
+                marked[ch - 'a'] = true;
                 sb.append(ch);
+
             }
             freq[ch - 'a']--;
         }
-
         return sb.toString();
     }
 }
