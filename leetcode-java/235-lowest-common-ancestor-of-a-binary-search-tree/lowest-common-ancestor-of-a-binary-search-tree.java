7ee9eca5fdbee9d2a06677d34f47ebc4a9ec631c
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-07-24 14:29:47 +0530

    Time: 6 ms (97.10%) | Memory: 47.6 MB (55.42%) - LeetSync

--- leetcode-java/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ leetcode-java/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,15 +10,28 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        int rootVal = root.val;
-        int pVal = p.val;
-        int qVal = q.val;
+        TreeNode curr = root;
 
-        if (pVal > rootVal && qVal > rootVal)
-            return lowestCommonAncestor(root.right, p, q);
-        else if (pVal < rootVal && qVal < rootVal)
-            return lowestCommonAncestor(root.left, p, q);
-        else
-            return root;
+        while (curr != null) {
+            // Case 1: Both p and q are strictly smaller than the current node.
+            // This means the split point (LCA) lies further down in the left subtree.
+            if ((curr.val > p.val) && (curr.val > q.val)) {
+                curr = curr.left;
+            } 
+            // Case 2: Both p and q are strictly larger than the current node.
+            // This means the split point (LCA) lies further down in the right subtree.
+            else if ((p.val > curr.val) && (q.val > curr.val)) {
+                curr = curr.right;
+            } 
+            // Case 3: Split point found!
+            // Either p and q are on opposite sides of curr, or curr is equal to p or q.
+            // In all these scenarios, curr is the Lowest Common Ancestor.
+            else {
+                return curr;
+            }
+        }
+
+        // Return null if the targets are not present in the tree
+        return null;
     }
 }
