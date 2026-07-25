aa0f59b3fdea80d6102eb58f00e826c700f38189
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-07-25 14:32:51 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (7.60%) - LeetSync

--- leetcode-java/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ leetcode-java/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -13,29 +13,59 @@
  *     }
  * }
  */
+
+
 class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-
-        if (root == null)
-            return res;
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while (!q.isEmpty()) {
-        List<Integer> lvlNodes = new ArrayList<>();
-            int n = q.size();
-            for (int i = 0; i < n; i++) {
-                TreeNode node = q.poll();
-                if (node.left != null)
-                    q.offer(node.left);
-                if (node.right != null)
-                    q.offer(node.right);
-                lvlNodes.add(node.val);
-            }
-            res.add(lvlNodes);
-        }
+        dfs(root, 0);
         return res;
     }
+
+    private void dfs(TreeNode node, int depth) {
+        if (node == null) {
+            return;
+        }
+
+        if (res.size() == depth) {
+            res.add(new ArrayList<>());
+        }
+
+        res.get(depth).add(node.val);
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
+    }
 }
+
+
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         if (root == null) {
+//             return res;
+//         }
+
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+
+//         while (!q.isEmpty()) {
+//             List<Integer> ls = new ArrayList<>();
+//             int n = q.size();
+//             for (int i = 0; i < n; i++) {
+//                 TreeNode node = q.poll();
+//                 if (node.left != null) {
+//                     q.offer(node.left);
+//                 }
+//                 if (node.right != null) {
+//                     q.offer(node.right);
+//                 }
+//                 ls.add(node.val);
+//             }
+//             res.add(ls);
+//         }
+
+//         return res;
+//     }
+// }
+
