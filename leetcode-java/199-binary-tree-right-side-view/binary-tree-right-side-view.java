20a9295833a6bece2ef0b6f749bfe5894bfd4f38
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-07-26 11:56:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (51.51%) - LeetSync

--- leetcode-java/199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ leetcode-java/199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -13,36 +13,62 @@
  *     }
  * }
  */
+
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) {
             return res;
         }
-        bfs(root, res);
+        dfs(root, 0, res);
         return res;
     }
 
-    private void bfs(TreeNode root, List<Integer> ls) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while (!q.isEmpty()) {
-            int n = q.size();
-            for (int i = 0; i < n; i++) {
-                TreeNode node = q.poll();
-                if (i == n - 1) {
-                    ls.add(node.val);
-                }
-
-                if (node.left != null) {
-                    q.offer(node.left);
-                }
-
-                if (node.right != null) {
-                    q.offer(node.right);
-                }
-            }
+    private void dfs(TreeNode root, int depth, List<Integer> ls) {
+        if (root == null) {
+            return;
         }
+
+        if (depth == ls.size()) {
+            ls.add(root.val);
+        }
+
+        dfs(root.right, depth + 1, ls);
+        dfs(root.left, depth + 1, ls);
     }
 }
+
+
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         if (root == null) {
+//             return res;
+//         }
+//         bfs(root, res);
+//         return res;
+//     }
+
+//     private void bfs(TreeNode root, List<Integer> ls) {
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+
+//         while (!q.isEmpty()) {
+//             int n = q.size();
+//             for (int i = 0; i < n; i++) {
+//                 TreeNode node = q.poll();
+//                 if (i == n - 1) {
+//                     ls.add(node.val);
+//                 }
+
+//                 if (node.left != null) {
+//                     q.offer(node.left);
+//                 }
+
+//                 if (node.right != null) {
+//                     q.offer(node.right);
+//                 }
+//             }
+//         }
+//     }
+// }
