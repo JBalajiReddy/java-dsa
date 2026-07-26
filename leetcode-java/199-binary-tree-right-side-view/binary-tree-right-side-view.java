080ae21311cd6ddf81fb800042ba9ca31bda1e36
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-07-26 11:35:28 +0530

    Time: 1 ms (70.83%) | Memory: 43.9 MB (14.82%) - LeetSync

--- leetcode-java/199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ leetcode-java/199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -13,14 +13,17 @@
  *     }
  * }
  */
-
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-
-        if (root == null)
+        if (root == null) {
             return res;
+        }
+        bfs(root, res);
+        return res;
+    }
 
+    private void bfs(TreeNode root, List<Integer> ls) {
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
 
@@ -28,16 +31,18 @@ class Solution {
             int n = q.size();
             for (int i = 0; i < n; i++) {
                 TreeNode node = q.poll();
-                if (i == n - 1)
-                    res.add(node.val);
+                if (i == n - 1) {
+                    ls.add(node.val);
+                }
 
-                if (node.left != null)
+                if (node.left != null) {
                     q.offer(node.left);
-                if (node.right != null)
-                    q.offer(node.right);
+                }
 
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
             }
         }
-        return res;
     }
 }
