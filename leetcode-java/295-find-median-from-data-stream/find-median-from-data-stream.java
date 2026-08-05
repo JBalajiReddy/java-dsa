649f484e0457ac0010bfb669d41b59e2799865a3
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-08-05 14:04:33 +0530

    Time: 213 ms (31.62%) | Memory: 111.2 MB (92.70%) - LeetSync

--- leetcode-java/295-find-median-from-data-stream/find-median-from-data-stream.java
+++ leetcode-java/295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,34 +1,23 @@
 class MedianFinder {
 
-    PriorityQueue<Integer> max;
-    PriorityQueue<Integer> min;
+    PriorityQueue<Integer> minH;
+    PriorityQueue<Integer> maxH;
 
     public MedianFinder() {
-        max = new PriorityQueue<>(Comparator.reverseOrder());
-        min = new PriorityQueue<>();
+        minH = new PriorityQueue<>(); //for 2nd half
+        maxH = new PriorityQueue<>(Collections.reverseOrder());  //for 1st half
     }
-
-    public void addNum(int num) {
-        // 1. Add the new number to the max-heap (smaller half).
-        // This is a temporary step; we might move it to the min-heap next.
-        max.offer(num);
-
-        // 2. Move the largest element from the max-heap to the min-heap.
-        // This ensures that the partitioning rule is maintained: the largest element
-        // of the smaller half is now correctly placed as the smallest in the larger half.
-        min.offer(max.poll());
-
-        // 3. Balance the heaps if necessary.
-        // If the min-heap (larger half) has more elements than the max-heap (smaller half),
-        // move the smallest element from `min` back to `max` to restore balance.
-        // This ensures the `max` heap is either equal in size or has one more element.
-        if (max.size() < min.size()) {
-            max.offer(min.poll());
+    
+    public void addNum(int num) {   
+        maxH.offer(num);
+        minH.offer(maxH.poll());
+        if (maxH.size() < minH.size()) {
+            maxH.offer(minH.poll());
         }
     }
-
+    
     public double findMedian() {
-        return (max.size() > min.size()) ? max.peek() : (max.peek() + min.peek()) / 2.0;
+        return (maxH.size() > minH.size()) ? maxH.peek() : (maxH.peek() + minH.peek()) / 2.0;
     }
 }
 
