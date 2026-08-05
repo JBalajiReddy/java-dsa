b09a3ee9abe64c561d18717f660614e2d690c0f6
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-08-05 14:42:32 +0530

    Time: 112 ms (52.29%) | Memory: 134.2 MB (44.93%) - LeetSync

--- leetcode-java/502-ipo/ipo.java
+++ leetcode-java/502-ipo/ipo.java
@@ -1,76 +1,29 @@
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        int n = profits.length;
-        
-        // 1. Store projects as {Capital, Profit} pairs in a 2D array
-        int[][] projects = new int[n][2];
+        int n = capital.length;
+        Integer[] indices = new Integer[n];
         for (int i = 0; i < n; i++) {
-            projects[i][0] = capital[i];
-            projects[i][1] = profits[i];
+            indices[i] = i;
         }
-        
-        // 2. Sort by Capital (Ascending). 
-        // This replaces the 'pqC' logic with a faster array sort.
-        Arrays.sort(projects, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        // 3. Max Heap for Profits (only stores Integers now)
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        int ptr = 0;
-        
+
+        Arrays.sort(indices, (a, b) -> Integer.compare(capital[a], capital[b]));
+
+        PriorityQueue<Integer> maxProfit = new PriorityQueue<>(Collections.reverseOrder());
+        int idx = 0;
+
         for (int i = 0; i < k; i++) {
-            // Move all affordable projects to the heap
-            // We use a pointer 'ptr' instead of polling from a queue
-            while (ptr < n && projects[ptr][0] <= w) {
-                pq.offer(projects[ptr][1]);
-                ptr++;
+            while (idx < n && capital[indices[idx]] <= w) {
+                maxProfit.offer(profits[indices[idx]]);
+                idx++;
             }
-            
-            // If we can't afford any new projects and have none in reserve
-            if (pq.isEmpty()) {
+
+            if (maxProfit.isEmpty()) {
                 break;
             }
-            
-            // Take the best profit
-            w += pq.poll();
+
+            w += maxProfit.poll();
         }
-        
+
         return w;
     }
 }
-
-// class Solution {
-//     class Pair {
-//         int profit, capital;
-//         Pair(int profit, int capital) {
-//             this.profit = profit;
-//             this.capital = capital;
-//         }
-//     }
-
-//     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-//         PriorityQueue<Pair> pqC = new PriorityQueue<>(
-//                 (a, b) -> {
-//                     return Integer.compare(a.capital, b.capital);
-//                 });
-
-//         PriorityQueue<Pair> pqP = new PriorityQueue<>(
-//                 (a, b) -> {
-//                     return Integer.compare(b.profit, a.profit);
-//                 });
-
-//         for (int i = 0; i < profits.length; i++) {
-//             pqC.offer(new Pair(profits[i], capital[i]));
-//         }
-
-//         for (int i = 0; i < k; i++) {
-//             while (!pqC.isEmpty() && pqC.peek().capital <= w) {
-//                 pqP.offer(pqC.poll());
-//             }
-//             if (pqP.isEmpty()) break;
-//             w += pqP.poll().profit;
-//         }
-
-//         return w;
-//     }
-// }
