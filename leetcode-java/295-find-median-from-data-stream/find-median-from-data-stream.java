ba2ccb015865722abc85d517831227a4b3da553d
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-08-05 14:08:11 +0530

    Time: 144 ms (60.48%) | Memory: 111.6 MB (24.34%) - LeetSync

--- leetcode-java/295-find-median-from-data-stream/find-median-from-data-stream.java
+++ leetcode-java/295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,21 +1,28 @@
 class MedianFinder {
-
     PriorityQueue<Integer> minH;
     PriorityQueue<Integer> maxH;
 
     public MedianFinder() {
-        minH = new PriorityQueue<>(); //for 2nd half
-        maxH = new PriorityQueue<>(Collections.reverseOrder());  //for 1st half
+        minH = new PriorityQueue<>(); // for 2nd half
+        maxH = new PriorityQueue<>(Collections.reverseOrder()); // for 1st half
     }
-    
-    public void addNum(int num) {   
-        maxH.offer(num);
-        minH.offer(maxH.poll());
-        if (maxH.size() < minH.size()) {
+
+    public void addNum(int num) {
+        // 1. Direct routing (1 heap op in most branch paths)
+        if (maxH.isEmpty() || num <= maxH.peek()) {
+            maxH.offer(num);
+        } else {
+            minH.offer(num);
+        }
+
+        // 2. Rebalance sizes if off by more than 1 (1 push + 1 pop = 2 heap ops)
+        if (maxH.size() > minH.size() + 1) {
+            minH.offer(maxH.poll());
+        } else if (minH.size() > maxH.size()) {
             maxH.offer(minH.poll());
         }
     }
-    
+
     public double findMedian() {
         return (maxH.size() > minH.size()) ? maxH.peek() : (maxH.peek() + minH.peek()) / 2.0;
     }
