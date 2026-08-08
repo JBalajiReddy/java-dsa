166de5100135f4d9795f130d3aef4ceb14baee93
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-08-08 15:04:20 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (46.13%) - LeetSync

--- leetcode-java/46-permutations/permutations.java
+++ leetcode-java/46-permutations/permutations.java
@@ -1,13 +1,43 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, nums, res);
+        return res;
+    }
+
+    private void backtrack(int start, int[] nums, List<List<Integer>> res) {
+        if (start == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int num : nums)
+                list.add(num);
+            res.add(list);
+            return;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i);
+            backtrack(start + 1, nums, res);
+            swap(nums, start, i); // Undo swap
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+class Solution_Backtracking {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
         backtrack(nums, visited, new ArrayList<>(), res);
         return res;
     }
 
     private void backtrack(
-        int[] nums, boolean[] visited, List<Integer> current, List<List<Integer>> res) {
+            int[] nums, boolean[] visited, List<Integer> current, List<List<Integer>> res) {
         // Base Case: Full permutation constructed
         if (current.size() == nums.length) {
             res.add(new ArrayList<>(current));
