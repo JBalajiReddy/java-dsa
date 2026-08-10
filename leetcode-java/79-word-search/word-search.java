95803c1775baafae7d926c98889f9dba8e6a007b
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-08-10 09:38:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (44.20%) - LeetSync

--- leetcode-java/79-word-search/word-search.java
+++ leetcode-java/79-word-search/word-search.java
@@ -1,36 +1,66 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int r = board.length;
-        int c = board[0].length;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (dfs(board, word, i, j, 0))
-                    return true;
+        int n = board.length;
+        int m = board[0].length;
+        int len = word.length();
+
+        if (len > n * m) {
+            return false;
+        }
+
+        // 1. Frequency Check Pruning
+        int[] boardCounts = new int[128];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                boardCounts[board[i][j]]++;
+            }
+        }
+
+        int[] wordCounts = new int[128];
+        for (int i = 0; i < len; i++) {
+            char c = word.charAt(i);
+            wordCounts[c]++;
+            if (wordCounts[c] > boardCounts[c]) {
+                return false; // Board lacks necessary characters
+            }
+        }
+
+        // 2. Search Direction Pruning (Reverse word if start char is more common than end char)
+        if (boardCounts[word.charAt(0)] > boardCounts[word.charAt(len - 1)]) {
+            word = new StringBuilder(word).reverse().toString();
+        }
+
+        // 3. Execution
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (backtrack(board, i, j, word, 0)) {
+                        return true;
+                    }
+                }
             }
         }
         return false;
     }
 
-    private boolean dfs(char[][] b, String w, int i, int j, int idx) {
-        int r = b.length;
-        int c = b[0].length;
-        if (i < 0 || i >= r || j < 0 || j >= c || b[i][j] != w.charAt(idx))
-            return false;
-
-        if (idx == w.length() - 1)
+    private boolean backtrack(char[][] b, int i, int j, String word, int s) {
+        if (s == word.length()) {
             return true;
+        }
 
-        char temp = b[i][j];
-        b[i][j] = '#'; //visited
+        if (i < 0 || i >= b.length || j < 0 || j >= b[0].length || b[i][j] != word.charAt(s)) {
+            return false;
+        }
 
-        boolean found = dfs(b, w, i + 1, j, idx + 1)
-                || dfs(b, w, i - 1, j, idx + 1)
-                || dfs(b, w, i, j + 1, idx + 1)
-                || dfs(b, w, i, j - 1, idx + 1);
+        char tmp = b[i][j];
+        b[i][j] = '#';
 
-        //backtrack
-        b[i][j] = temp;
+        boolean found = backtrack(b, i + 1, j, word, s + 1) ||
+                backtrack(b, i - 1, j, word, s + 1) ||
+                backtrack(b, i, j + 1, word, s + 1) ||
+                backtrack(b, i, j - 1, word, s + 1);
 
+        b[i][j] = tmp;
         return found;
     }
 }
