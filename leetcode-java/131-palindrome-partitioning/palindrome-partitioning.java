3be79d5e1bb41e08ab4779b2df5daa61294286f2
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-08-11 13:01:42 +0530

    Time: 7 ms (97.05%) | Memory: 65.4 MB (24.20%) - LeetSync

--- leetcode-java/131-palindrome-partitioning/palindrome-partitioning.java
+++ leetcode-java/131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,39 +1,48 @@
 class Solution {
-    private List<List<String>> res;
-
     public List<List<String>> partition(String s) {
-        res = new ArrayList<>();
-        backtrack(s, 0, new ArrayList<>());
+        int n = s.length();
+        List<List<String>> res = new ArrayList<>();
+        
+        // dp[i][j] will be true if substring s[i...j] is a palindrome
+        boolean[][] dp = new boolean[n][n];
+
+        // Step 1: Fill the DP table to precompute palindrome status
+        // We evaluate shorter substrings first so subproblem states (i+1, j-1) are ready
+        for (int end = 0; end < n; end++) {
+            for (int start = 0; start <= end; start++) {
+                // Characters at ends must match
+                if (s.charAt(start) == s.charAt(end)) {
+                    // Substring is a palindrome if it's 1-2 chars long OR inside substring is palindrome
+                    if (end - start <= 2 || dp[start + 1][end - 1]) {
+                        dp[start][end] = true;
+                    }
+                }
+            }
+        }
+
+        // Step 2: Backtrack using O(1) DP lookups
+        backtrack(s, 0, new ArrayList<>(), res, dp);
         return res;
     }
 
-    private void backtrack(String s, int start, List<String> ls) {
+    private void backtrack(String s, int start, List<String> currentList, List<List<String>> res, boolean[][] dp) {
+        // Base case: Reached end of string
         if (start == s.length()) {
-            res.add(new ArrayList<>(ls));
+            res.add(new ArrayList<>(currentList));
             return;
         }
 
         for (int end = start; end < s.length(); end++) {
-            String sub = s.substring(start, end + 1);
-            if (isPalindrome(sub)) {
-                ls.add(sub);
-
-                backtrack(s, end + 1, ls);
-
-                ls.remove(ls.size() - 1);
+            // O(1) lookup instead of O(K) two-pointer check
+            if (dp[start][end]) {
+                currentList.add(s.substring(start, end + 1));
+                
+                // Recurse for the remaining suffix
+                backtrack(s, end + 1, currentList, res, dp);
+                
+                // Backtrack (undo choice)
+                currentList.remove(currentList.size() - 1);
             }
         }
-    }
-
-    private boolean isPalindrome(String s) {
-        int start = 0, end = s.length() - 1;
-        while (start < end) {
-            if (s.charAt(start) != s.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
     }
 }
