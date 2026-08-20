c678011b8d58143f5063e17160fe88f23cb2c7f9
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-08-20 11:11:29 +0530

    Time: 1 ms (98.08%) | Memory: 46.8 MB (42.79%) - LeetSync

---
+++ leetcode-java/3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int m = n / 2;
+        res[0] = nums[0];
+        res[n - 1] = nums[1];
+        
+        int idx = 0, revIdx = n - 1;
+        for (int i = 2; i < n; i++) {
+            if (res[idx] > res[revIdx]) {
+                res[++idx] = nums[i];
+            } else {
+                res[--revIdx] = nums[i]; 
+            }
+        }
+
+        for (int j = revIdx, k = n - 1; j < k; j++, k--) {
+            int t = res[j];
+            res[j] = res[k];
+            res[k] = t;
+        }
+
+        return res;
+    }
+}
