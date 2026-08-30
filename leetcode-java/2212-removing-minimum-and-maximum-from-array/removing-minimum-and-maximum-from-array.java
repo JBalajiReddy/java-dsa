69bbf2118f7cf16dd9670f19a307889ea68b371b
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-08-30 12:27:56 +0530

    Time: 3 ms (61.82%) | Memory: 86.7 MB (60.18%) - LeetSync

--- leetcode-java/2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ leetcode-java/2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -2,28 +2,36 @@ class Solution {
     public int minimumDeletions(int[] nums) {
         int n = nums.length;
 
+        // Base case: if 2 or fewer elements, we must delete all elements anyway
         if (n <= 2) {
             return n;
         }
 
+        // 1. Find indices of minimum and maximum elements
         int minIdx = 0, maxIdx = 0;
         for (int i = 0; i < n; i++) {
             if (nums[i] > nums[maxIdx]) {
                 maxIdx = i;
-            } 
-            
+            }
             if (nums[i] < nums[minIdx]) {
                 minIdx = i;
             }
         }
 
+        // 2. Identify left-most and right-most target positions
         int left = Math.min(minIdx, maxIdx);
         int right = Math.max(minIdx, maxIdx);
 
+        // Option 1: Delete both from the front (up to right)
         int remFromLeft = right + 1;
+
+        // Option 2: Delete both from the back (up to left) -> FIXED HERE
         int remFromRight = n - left;
+
+        // Option 3: Delete left target from front, right target from back
         int remFromBoth = (left + 1) + (n - right);
 
+        // 3. Return minimum operations among the three options
         return Math.min(remFromBoth, Math.min(remFromLeft, remFromRight));
     }
 }
