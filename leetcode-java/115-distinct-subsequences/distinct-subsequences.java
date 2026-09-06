e543f7bd853ad1ae97a38a0cc3cd2d3d3678337b
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-09-06 21:23:42 +0530

    Time: 18 ms (76.88%) | Memory: 54.2 MB (73.01%) - LeetSync

--- leetcode-java/115-distinct-subsequences/distinct-subsequences.java
+++ leetcode-java/115-distinct-subsequences/distinct-subsequences.java
@@ -3,7 +3,7 @@ class Solution {
         int m = s.length(), n = t.length();
         int[][] dp = new int[m + 1][n + 1];
         for (int i = 0; i <= m; i++) dp[i][0] = 1; //j == 0
-        for (int j = 1; j <= n; j++) dp[0][j] = 0; //i == 0
+        // for (int j = 1; j <= n; j++) dp[0][j] = 0; //i == 0
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
@@ -19,26 +19,26 @@ class Solution {
 }
 
 
-// class Solution {
-//     public int numDistinct(String s, String t) {
-//         int m = s.length(), n = t.length();
-//         int[][] memo = new int[m][n];
-//         for (int mem[] : memo)
-//             Arrays.fill(mem, -1);
-//         return helper(m - 1, n - 1, s, t, memo);
-//     }
+class Solution_Memo {
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        int[][] memo = new int[m][n];
+        for (int mem[] : memo)
+            Arrays.fill(mem, -1);
+        return helper(m - 1, n - 1, s, t, memo);
+    }
 
-//     private int helper(int i, int j, String s, String t, int[][] dp) {
-//         if (j < 0)
-//             return 1;
-//         if (i < 0)
-//             return 0;
-//         if (dp[i][j] != -1)
-//             return dp[i][j];
+    private int helper(int i, int j, String s, String t, int[][] dp) {
+        if (j < 0)
+            return 1;
+        if (i < 0)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-//         if (s.charAt(i) == t.charAt(j))
-//             return dp[i][j] = helper(i - 1, j - 1, s, t, dp) + helper(i - 1, j, s, t, dp);
+        if (s.charAt(i) == t.charAt(j))
+            return dp[i][j] = helper(i - 1, j - 1, s, t, dp) + helper(i - 1, j, s, t, dp);
 
-//         return dp[i][j] = helper(i - 1, j, s, t, dp);
-//     }
-// }
+        return dp[i][j] = helper(i - 1, j, s, t, dp);
+    }
+}
