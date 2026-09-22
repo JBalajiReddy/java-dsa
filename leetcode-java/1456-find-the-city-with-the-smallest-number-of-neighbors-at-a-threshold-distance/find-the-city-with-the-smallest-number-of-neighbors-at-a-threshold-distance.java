1724f2971b0f0c73c8d91c30578d28bd125b4210
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-09-22 15:59:14 +0530

    Time: 7 ms (99.78%) | Memory: 46.4 MB (52.76%) - LeetSync

--- leetcode-java/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ leetcode-java/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,45 +1,55 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[][] dist = new int[n][n];
-        int inf = (int) (1e9);
+        int[][] cost = new int[n][n];
+
+        // 1. Initialize distance matrix
         for (int i = 0; i < n; i++) {
-            Arrays.fill(dist[i], inf);
-            dist[i][i] = 0;
-        }
-
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int w = edge[2];
-            dist[u][v] = w;
-            dist[v][u] = w;
-        }
-
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (dist[i][k] != inf && dist[k][j] != inf) {
-                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-                    }
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    cost[i][j] = 0;
+                } else {
+                    cost[i][j] = (int) 1e9;
                 }
             }
         }
 
-        int city = -1;
-        int cityCnt = n;
-        for (int i = 0; i < n; i++) {
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                if (dist[i][j] <= distanceThreshold)
-                    cnt++;
-            }
+        // 2. Populate direct edge weights
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], wt = e[2];
+            cost[u][v] = wt;
+            cost[v][u] = wt;
+        }
 
-            if (cnt <= cityCnt) {
-                city = i;
-                cityCnt = cnt;
+        // 3. Floyd-Warshall Algorithm
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    cost[i][j] = Math.min(cost[i][j], cost[i][k] + cost[k][j]);
+                }
             }
         }
 
-        return city;
+        // 4. Find the target city
+        int minCitiesCnt = (int) 1e9;
+        int res = -1;
+
+        for (int i = 0; i < n; i++) {
+            int cityCnt = 0; // RESET for each city 'i'
+
+            for (int j = 0; j < n; j++) {
+                // Do NOT count the city itself
+                if (i != j && cost[i][j] <= distanceThreshold) {
+                    cityCnt++;
+                }
+            }
+
+            // Tie-breaker: '<=' prefers the larger city index 'i'
+            if (cityCnt <= minCitiesCnt) {
+                minCitiesCnt = cityCnt;
+                res = i;
+            }
+        }
+
+        return res;
     }
 }
