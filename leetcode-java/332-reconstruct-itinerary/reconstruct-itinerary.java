d81ada9e25c2754fd982689a051a3edbf2b8a7e2
Jeedipalli Balaji Reddy <97969918+JBalajiReddy@users.noreply.github.com>
2026-09-24 23:14:26 +0530

    Time: 6 ms (76.11%) | Memory: 47 MB (67.89%) - LeetSync

--- leetcode-java/332-reconstruct-itinerary/reconstruct-itinerary.java
+++ leetcode-java/332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -8,10 +8,10 @@ class Solution {
         }
 
         List<String> res = new ArrayList<>();
-        
+
         // Step 2: Post-order Eulerian Path Traversal (Hierholzer's Algorithm)
         dfs("JFK", res, adj);
-        
+
         // Step 3: Reverse post-order result to get correct start-to-finish itinerary
         Collections.reverse(res);
         return res;
@@ -19,14 +19,58 @@ class Solution {
 
     private void dfs(String node, List<String> res, Map<String, PriorityQueue<String>> adj) {
         PriorityQueue<String> pq = adj.get(node);
-        
+
         // Destructively consume edges in lexicographical order
         while (pq != null && !pq.isEmpty()) {
             String nextDestination = pq.poll(); // Consume ticket
             dfs(nextDestination, res, adj);
         }
-        
+
         // Post-Order Step: Add node after exploring all outgoing flights
         res.add(node);
+    }
+}
+
+class Solution_Backtracking {
+    Map<String, List<String>> graph = new HashMap();
+    List<String> result = new ArrayList();
+    int numTickets = 0;
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+        numTickets = tickets.size();
+        for (List<String> ticket : tickets) {
+            graph.put(ticket.get(0), new ArrayList());
+        }
+        for (List<String> ticket : tickets) {
+            graph.get(ticket.get(0)).add(ticket.get(1));
+        }
+        for (Map.Entry<String, List<String>> edges : graph.entrySet()) {
+            Collections.sort(edges.getValue());
+        }
+
+        List<String> path = new ArrayList();
+        DFS("JFK", path);
+
+        return result;
+    }
+
+    public boolean DFS(String fromAirport, List<String> path) {
+        path.add(fromAirport);
+        if (path.size() == numTickets + 1) {
+            result = path;
+            return true;
+        }
+
+        List<String> neighbors = graph.get(fromAirport);
+        for (int i = 0; neighbors != null && i < neighbors.size(); i++) {
+            String toAirport = neighbors.get(i);
+            neighbors.remove(toAirport);
+            if (DFS(toAirport, path)) {
+                return true;
+            }
+            neighbors.add(i, toAirport);
+        }
+        path.remove(path.size() - 1);
+        return false;
     }
 }
